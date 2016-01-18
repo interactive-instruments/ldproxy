f5b32c0a52b6fe9fcd9e4e37f4554eca91a1a9e2
azahnen <zahnen@interactive-instruments.de>
2016-01-18 13:57:28 +0100

    seo fixes [#32]

---
+++ ldproxy-rest/src/main/java/de/ii/ldproxy/rest/filter/RestRootFilter.java
@@ -0,0 +1,59 @@
+package de.ii.ldproxy.rest.filter;
+
+import com.google.common.io.Resources;
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+import de.ii.ldproxy.output.html.DatasetView;
+import de.ii.xsf.logging.XSFLogger;
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.forgerock.i18n.slf4j.LocalizedLogger;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author zahnen
+ */
+@Component
+@Provides
+@Instantiate
+
+public class RestRootFilter implements ContainerResponseFilter {
+
+    private static final LocalizedLogger LOGGER = XSFLogger.getLogger(RestRootFilter.class);
+
+    @Override
+    public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
+
+        if (request.getPath().endsWith("services/app.css")) {
+            writeContent(response, "/css/app.css", "text/css; charset=utf-8");
+        } else if (request.getPath().endsWith("services/featureCollection.js")) {
+            writeContent(response, "/js/featureCollection.js", "application/javascript");
+        } else if (request.getPath().endsWith("services/featureDetails.js")) {
+            writeContent(response, "/js/featureDetails.js", "application/javascript");
+        }
+
+        return response;
+    }
+
+    private void writeContent(ContainerResponse response, final String file, final String mimeType) {
+        response.reset();
+
+        StreamingOutput stream = new StreamingOutput() {
+            @Override
+            public void write(OutputStream output) throws IOException, WebApplicationException {
+                Resources.asByteSource(Resources.getResource(DatasetView.class, file)).copyTo(output);
+            }
+        };
+
+        Response r = Response.ok(stream, mimeType).build();
+
+        response.setResponse(r);
+    }
+}
