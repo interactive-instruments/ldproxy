0eae79bf6125ba738a6a36da1fb8b9cf72743ebc
azahnen <zahnen@interactive-instruments.de>
2018-02-26 18:36:07 +0100

    update license headers

--- ldproxy-rest/src/main/java/de/ii/ldproxy/rest/wfs3/Wfs3Collections.java
+++ ldproxy-rest/src/main/java/de/ii/ldproxy/rest/wfs3/Wfs3Collections.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 interactive instruments GmbH
+ * Copyright 2018 interactive instruments GmbH
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
