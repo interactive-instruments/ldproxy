5a77ffcb822024f7a39c69fa87147b35dea150d2
Deniz Brix <deniz.brix@gmail.com>
2019-01-22 14:03:28 +0100

    add extensions manager

--- ldproxy-wfs3-api/src/main/java/de/ii/ldproxy/wfs3/api/ExtendableConfiguration.java
+++ ldproxy-wfs3-api/src/main/java/de/ii/ldproxy/wfs3/api/ExtendableConfiguration.java
@@ -1,7 +1,11 @@
 package de.ii.ldproxy.wfs3.api;
 
+import com.fasterxml.jackson.annotation.JsonMerge;
+import com.fasterxml.jackson.annotation.OptBoolean;
+
 import java.util.Map;
 
 public interface ExtendableConfiguration {
+    @JsonMerge(value = OptBoolean.FALSE)
     Map<String, ExtensionConfiguration> getExtensions();
 }
