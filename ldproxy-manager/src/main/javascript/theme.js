efa07686829b4f84aeafb6b16381070c2558b8a7
azahnen <zahnen@interactive-instruments.de>
2020-03-21 18:14:28 +0100

    remove old manager

--- ldproxy-manager/src/main/javascript/theme.js
+++
@@ -1,7 +0,0 @@
-import { customTheme as xtraplatformTheme } from "xtraplatform-manager/src/theme";
-import { deepMerge } from "grommet/utils";
-import { css } from 'styled-components';
-
-export const customTheme = deepMerge(xtraplatformTheme, {
-
-});
