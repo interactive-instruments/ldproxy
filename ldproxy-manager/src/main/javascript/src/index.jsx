7d7075fd8d87cecd7f1e346b72055a3e7384fe4e
azahnen <zahnen@interactive-instruments.de>
2020-04-04 17:09:54 +0200

    test plugins

--- ldproxy-manager/src/main/javascript/src/index.jsx
+++ ldproxy-manager/src/main/javascript/src/index.jsx
@@ -1,10 +1,10 @@
 import { render } from 'react-dom';
 import { launchApp } from 'feature-u';
 import { Manager, Theme } from '@xtraplatform/core';
-import { Services } from '@xtraplatform/services';
+//import { Services } from '@xtraplatform/services';
 
 export default launchApp({
-    features: [Manager, Theme, Services],
+    features: [Manager, Theme],
     aspects: [],
     registerRootAppElm: (app) => render(app, document.getElementById('root')),
 });
