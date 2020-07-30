f1a40d044948cec589ae539fd9acb74da3dfee26
azahnen <zahnen@interactive-instruments.de>
2020-07-30 17:24:38 +0200

    move AuthConfig to cfg.yml

--- ogcapi-stable/ogcapi-oas30/src/main/java/de/ii/ldproxy/wfs3/oas30/ExtendableOpenApiDefinition.java
+++ ogcapi-stable/ogcapi-oas30/src/main/java/de/ii/ldproxy/wfs3/oas30/ExtendableOpenApiDefinition.java
@@ -13,7 +13,8 @@ import de.ii.ldproxy.ogcapi.domain.Metadata;
 import de.ii.ldproxy.ogcapi.domain.OgcApiApiDataV2;
 import de.ii.ldproxy.ogcapi.domain.OgcApiExternalDocumentation;
 import de.ii.ldproxy.ogcapi.domain.URICustomizer;
-import de.ii.xtraplatform.auth.api.AuthConfig;
+import de.ii.xtraplatform.dropwizard.api.AuthConfig;
+import de.ii.xtraplatform.dropwizard.api.XtraPlatform;
 import io.swagger.v3.core.util.Json;
 import io.swagger.v3.core.util.Yaml;
 import io.swagger.v3.oas.models.ExternalDocumentation;
@@ -56,8 +57,8 @@ public class ExtendableOpenApiDefinition {
     private final AuthConfig authConfig;
     private Set<OpenApiExtension> openApiExtensions;
 
-    public ExtendableOpenApiDefinition(@Requires AuthConfig authConfig) {
-        this.authConfig = authConfig;
+    public ExtendableOpenApiDefinition(@Requires XtraPlatform xtraPlatform) {
+        this.authConfig = xtraPlatform.getConfiguration().auth;
     }
 
     private Set<OpenApiExtension> getOpenApiExtensions() {
