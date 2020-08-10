4d70f465d4834009d9b51f8b23a4ea284b7df460
Clemens Portele <portele@interactive-instruments.de>
2020-08-10 12:42:02 +0200

    fix merge

--- ogcapi-stable/ogcapi-common/src/main/java/de/ii/ldproxy/ogcapi/application/OgcApiExtensionRegistryImpl.java
+++ ogcapi-stable/ogcapi-common/src/main/java/de/ii/ldproxy/ogcapi/application/OgcApiExtensionRegistryImpl.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
