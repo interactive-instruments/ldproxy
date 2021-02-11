8089aea8db9ce91584db6eca205250d5dcf1eb78
Clemens Portele <portele@interactive-instruments.de>
2021-02-11 13:13:16 +0100

    update json schema validator to new version

--- ogcapi-stable/ogcapi-foundation/src/test/groovy/de/ii/ldproxy/ogcapi/infra/json/SchemaValidatorSpec.groovy
+++ ogcapi-stable/ogcapi-foundation/src/test/groovy/de/ii/ldproxy/ogcapi/infra/json/SchemaValidatorSpec.groovy
@@ -1,5 +1,6 @@
 package de.ii.ldproxy.ogcapi.infra.json
 
+import com.networknt.schema.SpecVersion
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -11,14 +12,43 @@ class SchemaValidatorSpec extends Specification {
         schemaValidator = new SchemaValidatorImpl()
     }
 
-    def "Validate a feature against its JSON schema"() {
+    def "Validate a feature against its JSON schema (draft 2019-09, explicit)"() {
+        given:
+        String schema = new File('src/test/resources/schema.json').getText()
+        String feature = new File('src/test/resources/feature.json').getText()
+        when:
+        Optional<String> result = schemaValidator.validate(schema, feature, SpecVersion.VersionFlag.V201909)
+        then:
+        result.isEmpty()
+    }
+
+    def "Validate a feature against its JSON schema (draft 2019-09, auto-detect)"() {
         given:
         String schema = new File('src/test/resources/schema.json').getText()
         String feature = new File('src/test/resources/feature.json').getText()
         when:
         Optional<String> result = schemaValidator.validate(schema, feature)
         then:
-        result.isPresent()
+        result.isEmpty()
     }
 
+    def "Validate a feature against its JSON schema (draft 07, explicit)"() {
+        given:
+        String schema = new File('src/test/resources/schema-draft07.json').getText()
+        String feature = new File('src/test/resources/feature.json').getText()
+        when:
+        Optional<String> result = schemaValidator.validate(schema, feature, SpecVersion.VersionFlag.V7)
+        then:
+        result.isEmpty()
+    }
+
+    def "Validate a feature against its JSON schema (draft 07, auto-detect)"() {
+        given:
+        String schema = new File('src/test/resources/schema-draft07.json').getText()
+        String feature = new File('src/test/resources/feature.json').getText()
+        when:
+        Optional<String> result = schemaValidator.validate(schema, feature)
+        then:
+        result.isEmpty()
+    }
 }
