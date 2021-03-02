1034cd669b2072f0bbebe8a44a371e05583ac137
azahnen <zahnen@interactive-instruments.de>
2021-03-02 17:18:07 +0100

    introduce supportsHighLoad

--- ogcapi-draft/ogcapi-tiles/src/main/java/de/ii/ldproxy/ogcapi/tiles/EndpointTileSingleCollection.java
+++ ogcapi-draft/ogcapi-tiles/src/main/java/de/ii/ldproxy/ogcapi/tiles/EndpointTileSingleCollection.java
@@ -106,7 +106,7 @@ public class EndpointTileSingleCollection extends EndpointSubCollection {
     @Override
     public boolean isEnabledForApi(OgcApiDataV2 apiData) {
         // currently no vector tiles support for WFS backends
-        if (providers.getFeatureProvider(apiData).getData().getFeatureProviderType().equals("WFS"))
+        if (!providers.getFeatureProvider(apiData).supportsHighLoad())
             return false;
 
         Optional<TilesConfiguration> extension = apiData.getExtension(TilesConfiguration.class);
