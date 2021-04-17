86da8836dba306fd188c19c6f71d2cb8d12c0350
azahnen <zahnen@interactive-instruments.de>
2021-04-17 12:12:45 +0200

    manager app name and theme

--- ldproxy-manager/src/main/javascript/src/index.jsx
+++ ldproxy-manager/src/main/javascript/src/index.jsx
@@ -1,5 +1,19 @@
-import { launch } from "@xtraplatform/manager";
+import { launch, app } from "@xtraplatform/manager";
 import { Services, Codelists } from "@xtraplatform/services";
 import Common from "@ogcapi/common";
+import { createFeature } from "feature-u";
 
-launch([Services, Codelists, Common]);
+const Ldproxy = createFeature({
+  name: "app-ldproxy",
+  fassets: {
+    // provided resources
+    define: {
+      [app("ldproxy")]: {
+        name: "ldproxy",
+        defaultTheme: "default",
+      },
+    },
+  },
+});
+
+launch([Services, Codelists, Common, Ldproxy]);
