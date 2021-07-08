c4b16c55fb66bc7eb5c819e984c7f86ad02f18bd
Clemens Portele <portele@interactive-instruments.de>
2021-07-08 16:57:09 +0200

    init JSON_FG module

---
+++ ogcapi-draft/ogcapi-features-json-fg/src/main/java/de/ii/ldproxy/ogcapi/features/jsonfg/domain/JsonFgConfiguration.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2021 interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.ii.ldproxy.ogcapi.features.jsonfg.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import de.ii.ldproxy.ogcapi.domain.ExtensionConfiguration;
+import org.immutables.value.Value;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Optional;
+
+@Value.Immutable
+@Value.Style(builder = "new")
+@JsonDeserialize(builder = ImmutableJsonFgConfiguration.Builder.class)
+public interface JsonFgConfiguration extends ExtensionConfiguration {
+
+    abstract class Builder extends ExtensionConfiguration.Builder {
+    }
+
+    @Nullable
+    Boolean getWhen();
+
+    @Nullable
+    Boolean getDescribedby();
+
+    @Override
+    default Builder getBuilder() {
+        return new ImmutableJsonFgConfiguration.Builder();
+    }
+}
