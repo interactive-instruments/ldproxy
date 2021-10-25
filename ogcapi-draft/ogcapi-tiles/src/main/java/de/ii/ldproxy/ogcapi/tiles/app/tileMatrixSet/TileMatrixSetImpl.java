interactive-instruments/ldproxy

commit bdd858697980dff560393d4dfe9baaa0d8e5b527

Load tile matrix set definitions (#520)
So far, each tile matrix set was implemented as a Java class. To be more flexible, the tile matrix sets are now loaded from their JSON definitions, either as resources or from api-resources/tile-matrix-sets.

Tile matrix set definitions for ETRS89/UTM32N have been added as resources.

Closes #505

Co-authored-by: azahnen <zahnen@interactive-instruments.de>

ogcapi-draft/ogcapi-tiles/src/main/java/de/ii/ldproxy/ogcapi/tiles/app/tileMatrixSet/TileMatrixSetImpl.java
@@ -1,0 +1,188 @@
+/**
+ * Copyright 2021 interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.ii.ldproxy.ogcapi.tiles.app.tileMatrixSet;
+
+import com.google.common.collect.ImmutableList;
+import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrix;
+import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSet;
+import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSetData;
+import de.ii.xtraplatform.crs.domain.BoundingBox;
+import de.ii.xtraplatform.crs.domain.EpsgCrs;
+import de.ii.xtraplatform.crs.domain.ImmutableBoundingBox;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+public class TileMatrixSetImpl implements TileMatrixSet {
+
+    private final TileMatrixSetData data;
+
+    public TileMatrixSetImpl(TileMatrixSetData data) {
+        this.data = data;
+    }
+
+    @Override
+    public String getId() {
+        return data.getId();
+    }
+
+    @Override
+    public Optional<URI> getURI() {
+        return data.getUri();
+    }
+
+    @Override
+    public Optional<String> getTitle() {
+        return data.getTitle();
+    }
+
+    @Override
+    public Optional<String> getDescription() {
+        return data.getDescription();
+    }
+
+    @Override
+    public List<String> getKeywords() {
+        return data.getKeywords();
+    }
+
+    @Override
+    public EpsgCrs getCrs() {
+        EpsgCrs crs;
+        try {
+            crs = EpsgCrs.fromString(data.getCrs());
+        } catch (Throwable e) {
+            throw new IllegalStateException(String.format("The CRS URI '%s' is invalid: %s", data.getCrs(), e.getMessage()), e);
+        }
+        return crs;
+    }
+
+    @Override
+    public List<String> getOrderedAxes() {
+        return data.getOrderedAxes();
+    }
+
+    @Override
+    public Optional<URI> getWellKnownScaleSet() {
+        return data.getWellKnownScaleSet();
+    }
+
+    @Override
+    public int getCols(int level) {
+        return data.getTileMatrices()
+                   .stream()
+                   .filter(tm -> tm.getTileLevel()==level)
+                   .findAny()
+                   .map(TileMatrix::getMatrixWidth)
+                   .orElse(0L).intValue();
+    }
+
+    @Override
+    public int getRows(int level) {
+        return data.getTileMatrices()
+                   .stream()
+                   .filter(tm -> tm.getTileLevel()==level)
+                   .findAny()
+                   .map(TileMatrix::getMatrixHeight)
+                   .orElse(0L).intValue();
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return data.getTileMatrices()
+                   .stream()
+                   .mapToInt(TileMatrix::getTileLevel)
+                   .max()
+                   .orElse(0);
+    }
+
+    @Override
+    public int getMinLevel() {
+        return data.getTileMatrices()
+                   .stream()
+                   .mapToInt(TileMatrix::getTileLevel)
+                   .min()
+                   .orElse(0);
+    }
+
+    @Override
+    public TileMatrixSetData getTileMatrixSetData() {
+        return data;
+    }
+
+    @Override
+    public double getInitialScaleDenominator() {
+        int minLevel = getMinLevel();
+        return data.getTileMatrices()
+                   .stream()
+                   .filter(tm -> tm.getTileLevel()==minLevel)
+                   .findAny()
+                   .map(tm -> tm.getScaleDenominator().doubleValue())
+                   .orElse(Double.NaN);
+    }
+
+    @Override
+    public int getInitialWidth() {
+        return getCols(getMinLevel());
+    }
+
+    @Override
+    public int getInitialHeight() {
+        return getRows(getMinLevel());
+    }
+
+    @Override
+    public BoundingBox getBoundingBox() {
+        return data.getBoundingBox().map(bbox -> new ImmutableBoundingBox.Builder().epsgCrs(bbox.getCrs().map(EpsgCrs::fromString).orElse(getCrs()))
+                                                                                   .xmin(bbox.getLowerLeft()[0].doubleValue())
+                                                                                   .ymin(bbox.getLowerLeft()[1].doubleValue())
+                                                                                   .xmax(bbox.getUpperRight()[0].doubleValue())
+                                                                                   .ymax(bbox.getUpperRight()[1].doubleValue())
+                                                                                   .build())
+                   .orElse(new ImmutableBoundingBox.Builder().epsgCrs(getCrs())
+                                                             .xmin(data.getTileMatrices()
+                                                                       .stream()
+                                                                       .filter(tm -> tm.getCornerOfOrigin().equals("topLeft"))
+                                                                       .mapToDouble(tm -> tm.getPointOfOrigin()[0].doubleValue())
+                                                                       .min().orElse(Double.NaN))
+                                                             .ymin(data.getTileMatrices()
+                                                                       .stream()
+                                                                       .filter(tm -> tm.getCornerOfOrigin().equals("topLeft"))
+                                                                       .mapToDouble(tm -> tm.getPointOfOrigin()[1].doubleValue() - tm.getCellSize().doubleValue() * tm.getTileHeight() * tm.getMatrixHeight())
+                                                                       .min().orElse(Double.NaN))
+                                                             .xmax(data.getTileMatrices()
+                                                                       .stream()
+                                                                       .filter(tm -> tm.getCornerOfOrigin().equals("topLeft"))
+                                                                       .mapToDouble(tm -> tm.getPointOfOrigin()[0].doubleValue() + tm.getCellSize().doubleValue() * tm.getTileWidth() * tm.getMatrixWidth())
+                                                                       .max().orElse(Double.NaN))
+                                                             .ymax(data.getTileMatrices()
+                                                                       .stream()
+                                                                       .filter(tm -> tm.getCornerOfOrigin().equals("topLeft"))
+                                                                       .mapToDouble(tm -> tm.getPointOfOrigin()[1].doubleValue())
+                                                                       .max().orElse(Double.NaN))
+                                                             .build());
+    }
+
+    @Override
+    public List<TileMatrix> getTileMatrices(int minLevel, int maxLevel) {
+        return data.getTileMatrices()
+                   .stream()
+                   .filter(tm -> tm.getTileLevel()>=minLevel && tm.getTileLevel()<=maxLevel)
+                   .collect(ImmutableList.toImmutableList());
+    }
+
+    @Override
+    public TileMatrix getTileMatrix(int level) {
+        return data.getTileMatrices()
+                   .stream()
+                   .filter(tm -> tm.getTileLevel()==level)
+                   .findAny()
+                   .orElse(null);
+    }
+}

ogcapi-draft/ogcapi-tiles/src/main/java/de/ii/ldproxy/ogcapi/tiles/app/CapabilityVectorTiles.java
@@ -16,7 +16,6 @@ import de.ii.ldproxy.ogcapi.domain.ExtensionRegistry;
 import de.ii.ldproxy.ogcapi.domain.FeatureTypeConfigurationOgcApi;
 import de.ii.ldproxy.ogcapi.domain.FormatExtension;
 import de.ii.ldproxy.ogcapi.domain.OgcApiDataV2;
-import de.ii.ldproxy.ogcapi.features.core.domain.FeaturesCoreConfiguration;
 import de.ii.ldproxy.ogcapi.features.core.domain.FeaturesCoreProviders;
 import de.ii.ldproxy.ogcapi.features.core.domain.FeaturesQuery;
 import de.ii.ldproxy.ogcapi.features.core.domain.SchemaInfo;
@@ -29,6 +28,7 @@ import de.ii.ldproxy.ogcapi.tiles.domain.TileFormatExtension;
 import de.ii.ldproxy.ogcapi.tiles.domain.TileSetFormatExtension;
 import de.ii.ldproxy.ogcapi.tiles.domain.TilesConfiguration;
 import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSet;
+import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSetRepository;
 import de.ii.xtraplatform.cql.domain.Cql;
 import de.ii.xtraplatform.store.domain.entities.ImmutableValidationResult;
 import de.ii.xtraplatform.store.domain.entities.ValidationResult;
@@ -62,13 +62,16 @@ public class CapabilityVectorTiles implements ApiBuildingBlock {
     private final FeaturesCoreProviders providers;
     private final FeaturesQuery queryParser;
     private final SchemaInfo schemaInfo;
+    private final TileMatrixSetRepository tileMatrixSetRepository;
 
     public CapabilityVectorTiles(@Requires ExtensionRegistry extensionRegistry, @Requires FeaturesQuery queryParser,
-                                 @Requires FeaturesCoreProviders providers, @Requires SchemaInfo schemaInfo) {
+                                 @Requires FeaturesCoreProviders providers, @Requires SchemaInfo schemaInfo,
+                                 @Requires TileMatrixSetRepository tileMatrixSetRepository) {
         this.extensionRegistry = extensionRegistry;
         this.queryParser = queryParser;
         this.providers = providers;
         this.schemaInfo = schemaInfo;
+        this.tileMatrixSetRepository = tileMatrixSetRepository;
     }
 
     @Override
@@ -109,10 +112,8 @@ public class CapabilityVectorTiles implements ApiBuildingBlock {
         if (Objects.nonNull(config.getZoomLevelsDerived()))
             return config.getZoomLevelsDerived().get(tileMatrixSetId);
 
-        Optional<TileMatrixSet> tileMatrixSet = extensionRegistry.getExtensionsForType(TileMatrixSet.class)
-                                                                 .stream()
-                                                                 .filter(tms -> tms.getId().equals(tileMatrixSetId) && tms.isEnabledForApi(apiData))
-                                                                 .findAny();
+        Optional<TileMatrixSet> tileMatrixSet = tileMatrixSetRepository.get(tileMatrixSetId)
+                                                                       .filter(tms -> config.getTileMatrixSets().contains(tms.getId()));
         return tileMatrixSet.map(matrixSet -> new ImmutableMinMax.Builder()
                 .min(matrixSet.getMinLevel())
                 .max(matrixSet.getMaxLevel())
@@ -120,10 +121,11 @@ public class CapabilityVectorTiles implements ApiBuildingBlock {
     }
 
     private MinMax getZoomLevels(OgcApiDataV2 apiData, String tileMatrixSetId) {
-        Optional<TileMatrixSet> tileMatrixSet = extensionRegistry.getExtensionsForType(TileMatrixSet.class)
-                                                                 .stream()
-                                                                 .filter(tms -> tms.getId().equals(tileMatrixSetId) && tms.isEnabledForApi(apiData))
-                                                                 .findAny();
+        Optional<TileMatrixSet> tileMatrixSet = tileMatrixSetRepository.get(tileMatrixSetId)
+                                                                       .filter(tms -> apiData.getExtension(TilesConfiguration.class)
+                                                                                        .map(TilesConfiguration::getTileMatrixSets)
+                                                                                        .filter(set -> set.contains(tms.getId()))
+                                                                                        .isPresent());
         return tileMatrixSet.map(matrixSet -> new ImmutableMinMax.Builder()
                 .min(matrixSet.getMinLevel())
                 .max(matrixSet.getMaxLevel())
@@ -203,10 +205,8 @@ public class CapabilityVectorTiles implements ApiBuildingBlock {
             if (Objects.nonNull(zoomLevels)) {
                 for (Map.Entry<String, MinMax> entry2 : zoomLevels.entrySet()) {
                     String tileMatrixSetId = entry2.getKey();
-                    Optional<TileMatrixSet> tileMatrixSet = extensionRegistry.getExtensionsForType(TileMatrixSet.class)
-                                                                             .stream()
-                                                                             .filter(tms -> tms.getId().equals(tileMatrixSetId) && tms.isEnabledForApi(apiData))
-                                                                             .findAny();
+                    Optional<TileMatrixSet> tileMatrixSet = tileMatrixSetRepository.get(tileMatrixSetId)
+                                                                                   .filter(tms -> config.getTileMatrixSets().contains(tms.getId()));
                     if (tileMatrixSet.isEmpty()) {
                         builder.addStrictErrors(MessageFormat.format("The configuration in the TILES module of collection ''{0}'' references a tile matrix set ''{1}'' that is not available in this API.", collectionId, tileMatrixSetId));
                     } else {

ogcapi-draft/ogcapi-tiles/src/main/java/de/ii/ldproxy/ogcapi/tiles/app/FeatureEncoderMVT.java
@@ -188,9 +188,6 @@ public class FeatureEncoderMVT extends FeatureObjectEncoder<PropertyMVT, Feature
     }
   }
 
-  //TODO: 2585449 feuerwehr size with master
-  // 2561287
-
   @Override
   public void onEnd(ModifiableContext context) {
     long mergerStart = System.nanoTime();

ogcapi-draft/ogcapi-tiles/src/main/java/de/ii/ldproxy/ogcapi/tiles/app/TileCacheImpl.java
@@ -25,6 +25,7 @@ import de.ii.ldproxy.ogcapi.tiles.domain.TileFormatExtension;
 import de.ii.ldproxy.ogcapi.tiles.domain.TileSet;
 import de.ii.ldproxy.ogcapi.tiles.domain.TilesConfiguration;
 import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSet;
+import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSetRepository;
 import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSetLimits;
 import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSetLimitsGenerator;
 import de.ii.xtraplatform.crs.domain.BoundingBox;
@@ -78,7 +79,7 @@ public class TileCacheImpl implements TileCache {
     private final SchemaInfo schemaInfo;
     private final ExtensionRegistry extensionRegistry;
     private final EntityRegistry entityRegistry;
-    private final TileMatrixSetLimitsGenerator tileMatrixSetLimitsGenerator;
+    private final TileMatrixSetRepository tileMatrixSetRepository;
 
     /**
      * set data directory
@@ -89,7 +90,7 @@ public class TileCacheImpl implements TileCache {
                          @Requires SchemaInfo schemaInfo,
                          @Requires ExtensionRegistry extensionRegistry,
                          @Requires EntityRegistry entityRegistry,
-                         @Requires TileMatrixSetLimitsGenerator tileMatrixSetLimitsGenerator) throws IOException {
+                         @Requires TileMatrixSetRepository tileMatrixSetRepository) throws IOException {
         // the ldproxy data directory, in development environment this would be ./build/data
         this.cacheStore = Paths.get(bundleContext.getProperty(DATA_DIR_KEY), CACHE_DIR)
                                .resolve(TILES_DIR_NAME);
@@ -98,7 +99,7 @@ public class TileCacheImpl implements TileCache {
         this.schemaInfo = schemaInfo;
         this.extensionRegistry = extensionRegistry;
         this.entityRegistry = entityRegistry;
-        this.tileMatrixSetLimitsGenerator = tileMatrixSetLimitsGenerator;
+        this.tileMatrixSetRepository = tileMatrixSetRepository;
         Files.createDirectories(cacheStore);
 
         mbtiles = new HashMap<>();
@@ -115,9 +116,7 @@ public class TileCacheImpl implements TileCache {
         ImmutableValidationResult.Builder builder = ImmutableValidationResult.builder()
                                                                              .mode(apiValidation);
 
-        Map<String, TileMatrixSet> tileMatrixSets = extensionRegistry.getExtensionsForType(TileMatrixSet.class)
-                                                                     .stream()
-                                                                     .collect(Collectors.toMap(TileMatrixSet::getId, tms -> tms));
+        Map<String, TileMatrixSet> tileMatrixSets = tileMatrixSetRepository.getAll();
         Optional<TilesConfiguration> config = apiData.getExtension(TilesConfiguration.class);
         if (config.isPresent()
                 && config.get().isEnabled()
@@ -462,14 +461,12 @@ public class TileCacheImpl implements TileCache {
     }
 
     private TileMatrixSet getTileMatrixSetById(String tileMatrixSetId) {
-        return extensionRegistry.getExtensionsForType(TileMatrixSet.class).stream()
-                                .filter(tms -> tms.getId().equals(tileMatrixSetId))
-                                .findAny()
-                                .orElseThrow(() -> new IllegalArgumentException("TileMatrixSet not found: "+tileMatrixSetId));
+        return tileMatrixSetRepository.get(tileMatrixSetId)
+                                      .orElseThrow(() -> new IllegalArgumentException("TileMatrixSet not found: "+tileMatrixSetId));
     }
 
     private List<TileMatrixSetLimits> getLimits(OgcApiDataV2 apiData, TileMatrixSet tileMatrixSet, MinMax minmax, Optional<String> collectionId, BoundingBox bbox) {
-        return tileMatrixSetLimitsGenerator.getTileMatrixSetLimits(bbox, tileMatrixSet, minmax);
+        return limitsGenerator.getTileMatrixSetLimits(bbox, tileMatrixSet, minmax);
     }
 
     private List<TileFormatExtension> getTileFormats(OgcApiDataV2 apiData, Optional<String> collectionId) {

ogcapi-draft/ogcapi-tiles/src/main/java/de/ii/ldproxy/ogcapi/tiles/app/TilesHelper.java
@@ -9,7 +9,6 @@ package de.ii.ldproxy.ogcapi.tiles.app;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import de.ii.ldproxy.ogcapi.domain.ExtensionConfiguration;
 import de.ii.ldproxy.ogcapi.domain.FeatureTypeConfigurationOgcApi;
 import de.ii.ldproxy.ogcapi.domain.Link;
 import de.ii.ldproxy.ogcapi.domain.OgcApiDataV2;
@@ -23,7 +22,6 @@ import de.ii.ldproxy.ogcapi.features.geojson.domain.JsonSchema;
 import de.ii.ldproxy.ogcapi.features.geojson.domain.JsonSchemaCache;
 import de.ii.ldproxy.ogcapi.features.geojson.domain.JsonSchemaDocument;
 import de.ii.ldproxy.ogcapi.features.geojson.domain.JsonSchemaObject;
-import de.ii.ldproxy.ogcapi.features.geojson.domain.SchemaDeriverReturnables;
 import de.ii.ldproxy.ogcapi.tiles.domain.ImmutableFields;
 import de.ii.ldproxy.ogcapi.tiles.domain.ImmutableTileLayer;
 import de.ii.ldproxy.ogcapi.tiles.domain.ImmutableTilePoint;
@@ -46,10 +44,9 @@ import de.ii.xtraplatform.crs.domain.OgcCrs;
 import de.ii.xtraplatform.features.domain.FeatureProvider2;
 import de.ii.xtraplatform.features.domain.FeatureSchema;
 import de.ii.xtraplatform.features.domain.SchemaBase;
-import de.ii.xtraplatform.features.domain.transform.ImmutablePropertyTransformation;
-import de.ii.xtraplatform.features.domain.transform.WithTransformationsApplied;
 import de.ii.xtraplatform.geometries.domain.SimpleFeatureGeometry;
 import de.ii.xtraplatform.store.domain.entities.EntityRegistry;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.AbstractMap;
@@ -111,7 +108,7 @@ public class TilesHelper {
                                                                                               BigDecimal.valueOf(boundingBox.getYmin()).setScale(7, RoundingMode.HALF_UP))
                                                                                    .upperRight(BigDecimal.valueOf(boundingBox.getXmax()).setScale(7, RoundingMode.HALF_UP),
                                                                                                BigDecimal.valueOf(boundingBox.getYmax()).setScale(7, RoundingMode.HALF_UP))
-                                                                                   .crsEpsg(OgcCrs.CRS84)
+                                                                                   .crs(OgcCrs.CRS84.toUriString())
                                                                                    .build()));
 
         if (zoomLevels.getDefault().isPresent() || Objects.nonNull(center)) {

ogcapi-draft/ogcapi-tiles/src/main/java/de/ii/ldproxy/ogcapi/tiles/app/TilesQueriesHandlerImpl.java
@@ -39,6 +39,7 @@ import de.ii.ldproxy.ogcapi.tiles.domain.TileSetsFormatExtension;
 import de.ii.ldproxy.ogcapi.tiles.domain.TilesConfiguration;
 import de.ii.ldproxy.ogcapi.tiles.domain.TilesQueriesHandler;
 import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSet;
+import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSetRepository;
 import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSetLimitsGenerator;
 import de.ii.xtraplatform.codelists.domain.Codelist;
 import de.ii.xtraplatform.crs.domain.CrsTransformer;
@@ -99,6 +100,7 @@ public class TilesQueriesHandlerImpl implements TilesQueriesHandler {
     private final TileCache tileCache;
     private final StaticTileProviderStore staticTileProviderStore;
     private final FeaturesCoreProviders providers;
+    private final TileMatrixSetRepository tileMatrixSetRepository;
 
     public TilesQueriesHandlerImpl(@Requires I18n i18n,
                                    @Requires CrsTransformerFactory crsTransformerFactory,
@@ -107,7 +109,8 @@ public class TilesQueriesHandlerImpl implements TilesQueriesHandler {
                                    @Requires TileMatrixSetLimitsGenerator limitsGenerator,
                                    @Requires TileCache tileCache,
                                    @Requires StaticTileProviderStore staticTileProviderStore,
-                                   @Requires FeaturesCoreProviders providers) {
+                                   @Requires FeaturesCoreProviders providers,
+                                   @Requires TileMatrixSetRepository tileMatrixSetRepository) {
         this.i18n = i18n;
         this.crsTransformerFactory = crsTransformerFactory;
         this.entityRegistry = entityRegistry;
@@ -116,6 +119,7 @@ public class TilesQueriesHandlerImpl implements TilesQueriesHandler {
         this.tileCache = tileCache;
         this.staticTileProviderStore = staticTileProviderStore;
         this.providers = providers;
+        this.tileMatrixSetRepository = tileMatrixSetRepository;
 
         this.queryHandlers = ImmutableMap.<Query, QueryHandler<? extends QueryInput>>builder()
                 .put(Query.TILE_SETS, QueryHandler.with(QueryInputTileSets.class, this::getTileSetsResponse))
@@ -600,10 +604,8 @@ public class TilesQueriesHandlerImpl implements TilesQueriesHandler {
     }
 
     private TileMatrixSet getTileMatrixSetById(String tileMatrixSetId) {
-        return extensionRegistry.getExtensionsForType(TileMatrixSet.class).stream()
-                    .filter(tms -> tms.getId().equals(tileMatrixSetId))
-                    .findAny()
-                    .orElseThrow(() -> new ServerErrorException("TileMatrixSet not found: "+tileMatrixSetId, 500));
+        return tileMatrixSetRepository.get(tileMatrixSetId)
+                                      .orElseThrow(() -> new ServerErrorException("TileMatrixSet not found: "+tileMatrixSetId, 500));
     }
 
     private ResultReduced<byte[]> generateTile(

ogcapi-draft/ogcapi-tiles/src/main/java/de/ii/ldproxy/ogcapi/tiles/app/html/TileSetsFormatHtml.java
@@ -22,6 +22,7 @@ import de.ii.ldproxy.ogcapi.html.domain.NavigationDTO;
 import de.ii.ldproxy.ogcapi.tiles.domain.TileSets;
 import de.ii.ldproxy.ogcapi.tiles.domain.TileSetsFormatExtension;
 import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSet;
+import de.ii.ldproxy.ogcapi.tiles.domain.tileMatrixSet.TileMatrixSetRepository;
 import io.swagger.v3.oas.models.media.StringSchema;
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Instantiate;
@@ -32,7 +33,6 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Component
 @Provides
@@ -46,10 +46,13 @@ public class TileSetsFormatHtml implements TileSetsFormatExtension {
 
     private final ExtensionRegistry extensionRegistry;
     private final I18n i18n;
+    private final TileMatrixSetRepository tileMatrixSetRepository;
 
-    public TileSetsFormatHtml(@Requires ExtensionRegistry extensionRegistry, @Requires I18n i18n) {
+    public TileSetsFormatHtml(@Requires ExtensionRegistry extensionRegistry, @Requires I18n i18n,
+                              @Requires TileMatrixSetRepository tileMatrixSetRepository) {
         this.extensionRegistry = extensionRegistry;
         this.i18n = i18n;
+        this.tileMatrixSetRepository = tileMatrixSetRepository;
     }
 
     @Override
@@ -124,9 +127,7 @@ public class TileSetsFormatHtml implements TileSetsFormatExtension {
                                                  .getExtension(HtmlConfiguration.class)
                                                  .orElse(null);
 
-        Map<String, TileMatrixSet> tileMatrixSets = extensionRegistry.getExtensionsForType(TileMatrixSet.class)
-                .stream()
-                .collect(Collectors.toMap(TileMatrixSet::getId, tms -> tms));
+        Map<String, TileMatrixSet> tileMatrixSets = tileMatrixSetRepository.getAll();
 
         return new TileSetsView(api.getData(), tiles, collectionId, tileMatrixSets, breadCrumbs, requestContext.getStaticUrlPrefix(), htmlConfig, isNoIndexEnabledForApi(api.getData()), requestContext.getUriCustomizer(), i18n, requestContext.getLanguage());
     }

commit ea4e392a60fadc92e9cae91474fe06ec03539f55

fix tms bbox
Correct an error introduced in #520. If the tile matrix set is based on CRS84, there is no need for a transformation.

ogcapi-draft/ogcapi-tiles/src/main/java/de/ii/ldproxy/ogcapi/tiles/app/tileMatrixSet/TileMatrixSetImpl.java
@@ -176,6 +176,8 @@ public class TileMatrixSetImpl implements TileMatrixSet {
 
     @Override
     public BoundingBox getBoundingBoxCrs84(CrsTransformerFactory crsTransformerFactory) throws CrsTransformationException {
+        if (getCrs().equals(OgcCrs.CRS84))
+            return getBoundingBox();
         CrsTransformer crsTransformer = crsTransformerFactory.getTransformer(getCrs(), OgcCrs.CRS84)
                                                              .orElseThrow(() -> new IllegalStateException(String.format("Could not transform the bounding box of tile matrix set '%s' to CRS84.", getId())));
         return crsTransformer.transformBoundingBox(getBoundingBox());