c71149938c95e87fc9668345fb84fba584b875fa
azahnen <zahnen@interactive-instruments.de>
2022-02-21 17:51:27 +0100

    license headers

--- ogcapi-draft/ogcapi-features-custom-extensions/src/main/java/de/ii/ogcapi/features/custom/extensions/domain/package-info.java
+++ ogcapi-draft/ogcapi-features-custom-extensions/src/main/java/de/ii/ogcapi/features/custom/extensions/domain/package-info.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2022 interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 @AutoModule(single = true, encapsulate = true)
 package de.ii.ogcapi.features.custom.extensions.domain;
 
