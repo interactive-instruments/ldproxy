2e218d21bccd8f3f21e349deadee3ebd2b173952
Ege Cabuk <94187749+CabukEge@users.noreply.github.com>
2022-12-21 12:26:49 +0100

    Merged master

--- ogcapi-stable/ogcapi-collections/src/main/java/de/ii/ogcapi/collections/app/html/OgcApiCollectionView.java
+++ ogcapi-stable/ogcapi-collections/src/main/java/de/ii/ogcapi/collections/app/html/OgcApiCollectionView.java
@@ -239,9 +239,7 @@ public abstract class OgcApiCollectionView extends OgcApiDatasetView {
   }
 
   public Optional<Link> getMap() {
-    return getLinks().stream()
-        .filter(link -> Objects.equals(link.getRel(), "ldp-map"))
-        .findFirst();
+    return getLinks().stream().filter(link -> Objects.equals(link.getRel(), "ldp-map")).findFirst();
   }
 
   public Optional<String> getSchemaOrgDataset() {
