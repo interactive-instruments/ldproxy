935d8967709814dc10a0fac3ba88e9e499518ad3
Andreas Zahnen <zahnen@interactive-instruments.de>
2023-02-16 15:24:56 +0100

    add todos

---
+++ ogcapi-stable/ogcapi-html/src/main/javascript/src/components/FilterEditor/hooks.js
@@ -0,0 +1,5 @@
+export const useApiInfo = (relativeUrl) => {
+  // TODO: custom hook that encapsulates fetch logic from FetchingPropertiesEnum + FetchingSpatialTemporal
+  // return the plain json object, extraction of fields/codes/bounds etc. goes to util.js
+  return relativeUrl;
+};
---
+++ ogcapi-stable/ogcapi-html/src/main/javascript/src/components/FilterEditor/util.js
@@ -0,0 +1,13 @@
+export const extractFields = (queryables) => {
+  console.log(queryables);
+
+  return { fistname: "Vorname" };
+};
+
+export const extractInterval = (collection) => {
+  console.log(collection);
+
+  return { start: 0, end: 0 };
+};
+
+// TODO: other extractions
