interactive-instruments/ldproxy

commit b69af9778bcf91f429832a2e13c5fa5b7475a53d

Add support for glTF 2.0 and 3D Tiles 1.1 (#901)
- add support for glTF 2.0 and 3D Tiles 1.1
- address review comments
  - support hitsOnly for GeoJSON, JSON-FG and glTF output
  - support OGC-numberMatched, OGC-numberReturned headers for hitsOnly requests
  - update subtree requests
  - prepare support for resultType=hits
  - move code to store 3D Tiles files in the cache to 3D Tiles building block
  - remove dead code

---------

Co-authored-by: Andreas Zahnen <zahnen@interactive-instruments.de>

ogcapi-draft/ogcapi-features-gltf/src/main/java/de/ii/ogcapi/features/gltf/app/FeaturesFormatGltfBinary.java
@@ -1,0 +1,414 @@
+/*
+ * Copyright 2022 interactive instruments GmbH
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package de.ii.ogcapi.features.gltf.app;
+
+import com.github.azahnen.dagger.annotations.AutoBind;
+import com.google.common.collect.ImmutableMap;
+import de.ii.ogcapi.features.core.domain.FeatureFormatExtension;
+import de.ii.ogcapi.features.core.domain.FeatureTransformationContext;
+import de.ii.ogcapi.features.core.domain.FeaturesCoreProviders;
+import de.ii.ogcapi.features.core.domain.FeaturesCoreValidation;
+import de.ii.ogcapi.features.gltf.domain.FeatureTransformationContextGltf;
+import de.ii.ogcapi.features.gltf.domain.GltfAsset;
+import de.ii.ogcapi.features.gltf.domain.GltfConfiguration;
+import de.ii.ogcapi.features.gltf.domain.GltfSchema;
+import de.ii.ogcapi.features.gltf.domain.ImmutableFeatureTransformationContextGltf;
+import de.ii.ogcapi.features.gltf.domain.Metadata3dSchemaCache;
+import de.ii.ogcapi.foundation.domain.ApiMediaType;
+import de.ii.ogcapi.foundation.domain.ApiMediaTypeContent;
+import de.ii.ogcapi.foundation.domain.ExtensionConfiguration;
+import de.ii.ogcapi.foundation.domain.FeatureTypeConfigurationOgcApi;
+import de.ii.ogcapi.foundation.domain.ImmutableApiMediaType;
+import de.ii.ogcapi.foundation.domain.ImmutableApiMediaTypeContent;
+import de.ii.ogcapi.foundation.domain.OgcApi;
+import de.ii.ogcapi.foundation.domain.OgcApiDataV2;
+import de.ii.xtraplatform.codelists.domain.Codelist;
+import de.ii.xtraplatform.crs.domain.CrsTransformer;
+import de.ii.xtraplatform.crs.domain.CrsTransformerFactory;
+import de.ii.xtraplatform.crs.domain.EpsgCrs;
+import de.ii.xtraplatform.crs.domain.OgcCrs;
+import de.ii.xtraplatform.features.domain.FeatureSchema;
+import de.ii.xtraplatform.features.domain.FeatureTokenEncoder;
+import de.ii.xtraplatform.features.domain.SchemaBase.Type;
+import de.ii.xtraplatform.features.domain.SchemaConstraints;
+import de.ii.xtraplatform.features.domain.transform.PropertyTransformation;
+import de.ii.xtraplatform.geometries.domain.SimpleFeatureGeometry;
+import de.ii.xtraplatform.services.domain.ServicesContext;
+import de.ii.xtraplatform.store.domain.entities.EntityRegistry;
+import de.ii.xtraplatform.store.domain.entities.ImmutableValidationResult;
+import de.ii.xtraplatform.store.domain.entities.ValidationResult;
+import de.ii.xtraplatform.store.domain.entities.ValidationResult.MODE;
+import io.swagger.v3.oas.models.media.BinarySchema;
+import io.swagger.v3.oas.models.media.Schema;
+import java.io.IOException;
+import java.net.URI;
+import java.text.MessageFormat;
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.ws.rs.core.MediaType;
+
+@AutoBind
+@Singleton
+public class FeaturesFormatGltfBinary implements FeatureFormatExtension {
+
+  public static final ApiMediaType MEDIA_TYPE =
+      new ImmutableApiMediaType.Builder()
+          .type(new MediaType("model", "gltf-binary"))
+          .label("glTF-Binary")
+          .parameter("glb")
+          .build();
+
+  public static final ApiMediaType COLLECTION_MEDIA_TYPE =
+      new ImmutableApiMediaType.Builder()
+          .type(new MediaType("application", "json"))
+          .label("JSON")
+          .parameter("json")
+          .build();
+
+  public static final Schema<?> SCHEMA = new BinarySchema();
+  public static final String SCHEMA_REF = "#/components/schemas/glTF";
+
+  private final FeaturesCoreProviders providers;
+  private final EntityRegistry entityRegistry;
+  private final FeaturesCoreValidation featuresCoreValidator;
+  private final CrsTransformer toEcef;
+  private final URI serviceUri;
+  private final Metadata3dSchemaCache schemaCache;
+
+  @Inject
+  public FeaturesFormatGltfBinary(
+      FeaturesCoreProviders providers,
+      EntityRegistry entityRegistry,
+      FeaturesCoreValidation featuresCoreValidator,
+      CrsTransformerFactory crsTransformerFactory,
+      ServicesContext servicesContext,
+      Metadata3dSchemaCache schemaCache) {
+    this.providers = providers;
+    this.entityRegistry = entityRegistry;
+    this.featuresCoreValidator = featuresCoreValidator;
+    this.toEcef =
+        crsTransformerFactory
+            .getTransformer(OgcCrs.CRS84h, EpsgCrs.of(4978))
+            .orElseThrow(
+                () ->
+                    new IllegalStateException(
+                        "Could not create a CRS transformer from CRS84h to EPSG:4978."));
+    this.serviceUri = servicesContext.getUri();
+    this.schemaCache = schemaCache;
+  }
+
+  @Override
+  public Class<? extends ExtensionConfiguration> getBuildingBlockConfigurationType() {
+    return GltfConfiguration.class;
+  }
+
+  @Override
+  public boolean canSupportTransactions() {
+    return false;
+  }
+
+  @Override
+  public ApiMediaTypeContent getContent() {
+    // TODO Should we describe the schema used in the binary file? As an OpenAPI schema?
+    return new ImmutableApiMediaTypeContent.Builder()
+        .schema(SCHEMA)
+        .schemaRef(SCHEMA_REF)
+        .ogcApiMediaType(getMediaType())
+        .build();
+  }
+
+  @Override
+  public EpsgCrs getContentCrs(EpsgCrs targetCrs) {
+    return EpsgCrs.of(4978);
+  }
+
+  @Override
+  public ValidationResult onStartup(OgcApi api, MODE apiValidation) {
+
+    // no additional operational checks for now, only validation; we can stop, if no validation is
+    // requested
+    if (apiValidation == MODE.NONE) {
+      return ValidationResult.of();
+    }
+
+    ImmutableValidationResult.Builder builder =
+        ImmutableValidationResult.builder().mode(apiValidation);
+
+    final OgcApiDataV2 apiData = api.getData();
+    final Map<String, FeatureSchema> featureSchemas = providers.getFeatureSchemas(apiData);
+
+    validateSchema(api, builder);
+
+    // get Gltf configurations to process
+    Map<String, GltfConfiguration> configurationMap = getGltfConfigurationMap(apiData);
+
+    Map<String, Collection<String>> propertyNamesWithTransformations =
+        getPropertyNamesWithTransformations(configurationMap);
+
+    for (Map.Entry<String, Collection<String>> stringCollectionEntry :
+        featuresCoreValidator
+            .getInvalidPropertyKeys(propertyNamesWithTransformations, featureSchemas)
+            .entrySet()) {
+      for (String property : stringCollectionEntry.getValue()) {
+        builder.addStrictErrors(
+            MessageFormat.format(
+                "A transformation for property ''{0}'' in collection ''{1}'' is invalid, because the property was not found in the provider schema.",
+                property, stringCollectionEntry.getKey()));
+      }
+    }
+
+    validateTransformations(builder, configurationMap);
+
+    return builder.build();
+  }
+
+  private void validateTransformations(
+      ImmutableValidationResult.Builder builder, Map<String, GltfConfiguration> configurationMap) {
+    Set<String> codelists =
+        entityRegistry.getEntitiesForType(Codelist.class).stream()
+            .map(Codelist::getId)
+            .collect(Collectors.toUnmodifiableSet());
+    for (Map.Entry<String, GltfConfiguration> entry : configurationMap.entrySet()) {
+      String collectionId = entry.getKey();
+      for (Map.Entry<String, List<PropertyTransformation>> entry2 :
+          entry.getValue().getTransformations().entrySet()) {
+        String property = entry2.getKey();
+        for (PropertyTransformation transformation : entry2.getValue()) {
+          transformation.validate(builder, collectionId, property, codelists);
+        }
+      }
+    }
+  }
+
+  private Map<String, Collection<String>> getPropertyNamesWithTransformations(
+      Map<String, GltfConfiguration> configurationMap) {
+    Map<String, Collection<String>> keyMap =
+        configurationMap.entrySet().stream()
+            .map(
+                entry ->
+                    new AbstractMap.SimpleImmutableEntry<>(
+                        entry.getKey(), entry.getValue().getTransformations().keySet()))
+            .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
+    return keyMap;
+  }
+
+  private Map<String, GltfConfiguration> getGltfConfigurationMap(OgcApiDataV2 apiData) {
+    Map<String, GltfConfiguration> configurationMap =
+        apiData.getCollections().entrySet().stream()
+            .map(
+                entry -> {
+                  final FeatureTypeConfigurationOgcApi collectionData = entry.getValue();
+                  final GltfConfiguration config =
+                      collectionData.getExtension(GltfConfiguration.class).orElse(null);
+                  if (Objects.isNull(config)) {
+                    return null;
+                  }
+                  return new AbstractMap.SimpleImmutableEntry<>(entry.getKey(), config);
+                })
+            .filter(Objects::nonNull)
+            .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
+    return configurationMap;
+  }
+
+  private void validateSchema(OgcApi api, ImmutableValidationResult.Builder builder) {
+    if (api.getData().getCollections().size() != 1) {
+      builder.addErrors(
+          MessageFormat.format(
+              "Support for glTF currently requires an API with a single feature collection. Found '{0}' collections.",
+              api.getData().getCollections().size()));
+      return;
+    }
+
+    FeatureTypeConfigurationOgcApi collectionData =
+        api.getData().getCollections().values().iterator().next();
+    FeatureSchema featureType =
+        providers.getFeatureSchema(api.getData(), collectionData).orElse(null);
+    if (Objects.isNull(featureType)) {
+      builder.addErrors(
+          MessageFormat.format(
+              "No schema for feature type '{0}' was found.", collectionData.getId()));
+    } else {
+      boolean atLeastOneLoD = false;
+      if (featureType.getPropertyMap().containsKey("lod1Solid")) {
+        atLeastOneLoD =
+            validateSolid(builder, featureType.getPropertyMap().get("lod1Solid"), "lod1Solid");
+      }
+      if (featureType.getPropertyMap().containsKey("lod2Solid")
+          && validateSolid(builder, featureType.getPropertyMap().get("lod2Solid"), "lod2Solid")) {
+        atLeastOneLoD =
+            validateSurfaces(builder, featureType.getPropertyMap().get("surfaces"), "surfaces");
+      }
+      if (!atLeastOneLoD) {
+        builder.addErrors(
+            "The feature type of the API must be a building features of LoD1 or LoD2.");
+      }
+    }
+  }
+
+  private boolean validateSolid(
+      ImmutableValidationResult.Builder builder, FeatureSchema solid, String propertyName) {
+    boolean valid =
+        Objects.nonNull(solid)
+            && solid.isSpatial()
+            && solid
+                .getGeometryType()
+                .filter(gt -> gt.equals(SimpleFeatureGeometry.MULTI_POLYGON))
+                .isPresent();
+    if (!valid) {
+      builder.addErrors(
+          MessageFormat.format(
+              "Feature property '{0}' must be a MULTI_POLYGON with the constraints 'composite' and 'closed'. Found: {1}",
+              propertyName, Objects.nonNull(solid) ? solid.toString() : "null"));
+      return false;
+    }
+    valid =
+        solid.getConstraints().stream().anyMatch(SchemaConstraints::isComposite)
+            && solid.getConstraints().stream().anyMatch(SchemaConstraints::isClosed);
+    if (!valid) {
+      builder.addStrictErrors(
+          MessageFormat.format(
+              "Feature property '{0}' must be a MULTI_POLYGON with the constraints 'composite' and 'closed'. Found: {1}",
+              propertyName, solid.toString()));
+    }
+    return true;
+  }
+
+  private boolean validateSurfaces(
+      ImmutableValidationResult.Builder builder, FeatureSchema surfaces, String propertyName) {
+    //noinspection ConstantConditions
+    boolean valid =
+        Objects.nonNull(surfaces)
+            && surfaces.getType().equals(Type.OBJECT_ARRAY)
+            && surfaces.getPropertyMap().containsKey("surfaceType")
+            && surfaces.getPropertyMap().get("surfaceType").getType().equals(Type.STRING)
+            && surfaces.getPropertyMap().containsKey("lod2MultiSurface")
+            && surfaces
+                .getPropertyMap()
+                .get("lod2MultiSurface")
+                .getGeometryType()
+                .filter(gt -> gt.equals(SimpleFeatureGeometry.MULTI_POLYGON))
+                .isPresent();
+    if (!valid) {
+      builder.addErrors(
+          MessageFormat.format(
+              "Feature property '{0}' must be an array of objects with the string property 'surfaceType' and the MULTI_POLYGON property 'lod2MultiSurface'. Found: {1}",
+              propertyName, Objects.nonNull(surfaces) ? surfaces.toString() : "null"));
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public ApiMediaType getMediaType() {
+    return MEDIA_TYPE;
+  }
+
+  @Override
+  public ApiMediaType getCollectionMediaType() {
+    return COLLECTION_MEDIA_TYPE;
+  }
+
+  @Override
+  public boolean canEncodeFeatures() {
+    return true;
+  }
+
+  @Override
+  public Optional<FeatureTokenEncoder<?>> getFeatureEncoder(
+      FeatureTransformationContext transformationContext, Optional<Locale> language) {
+
+    // CRS must be CRS84h
+    EpsgCrs crs = transformationContext.getTargetCrs();
+    if (!OgcCrs.CRS84h.equals(crs)) {
+      throw new IllegalArgumentException(
+          String.format(
+              "glTF-Binary requires a 3D coordinate reference system. Found: '%s'",
+              crs.toUriString()));
+    }
+
+    OgcApiDataV2 apiData = transformationContext.getApiData();
+    String collectionId = transformationContext.getCollectionId();
+
+    URI schemaUri =
+        URI.create(
+            String.format(
+                "%s/%s/collections/%s/gltf/schema?f=json",
+                serviceUri.toString(), apiData.getId(), collectionId));
+
+    GltfSchema gltfSchema =
+        schemaCache.getSchema(
+            transformationContext.getFeatureSchema().orElseThrow(),
+            apiData,
+            collectionId,
+            entityRegistry.getEntitiesForType(Codelist.class));
+
+    FeatureTransformationContextGltf transformationContextGltf =
+        ImmutableFeatureTransformationContextGltf.builder()
+            .from(transformationContext)
+            .clampToEllipsoid(
+                "true"
+                    .equalsIgnoreCase(
+                        transformationContext
+                            .getOgcApiRequest()
+                            .getParameters()
+                            .get("clampToEllipsoid")))
+            .crsTransformerCrs84hToEcef(toEcef)
+            .schemaUri(schemaUri)
+            .gltfSchema(gltfSchema)
+            .gltfConfiguration(
+                apiData.getExtension(GltfConfiguration.class, collectionId).orElseThrow())
+            .build();
+
+    return Optional.of(new FeatureEncoderGltf(transformationContextGltf));
+  }
+
+  @Override
+  public boolean supportsHitsOnly() {
+    return true;
+  }
+
+  @Override
+  public Optional<Long> getNumberMatched(Object content) {
+    return getMetadata(content, "numberMatched");
+  }
+
+  @Override
+  public Optional<Long> getNumberReturned(Object content) {
+    return getMetadata(content, "numberReturned");
+  }
+
+  private Optional<Long> getMetadata(Object content, String key) {
+    if (content instanceof byte[]) {
+      try {
+        GltfAsset asset = GltfAsset.of((byte[]) content);
+        if (asset.getAsset().getExtras().containsKey(key)) {
+          Object value = asset.getAsset().getExtras().get(key);
+          if (value instanceof Long) {
+            return Optional.of((Long) value);
+          } else if (value instanceof Integer) {
+            return Optional.of(((Integer) value).longValue());
+          } else if (value instanceof String) {
+            return Optional.of(Long.parseLong((String) value));
+          }
+        }
+      } catch (IOException e) {
+        return Optional.empty();
+      }
+    }
+    return Optional.empty();
+  }
+}

ogcapi-draft/ogcapi-features-jsonfg/src/main/java/de/ii/ogcapi/features/jsonfg/domain/FeaturesFormatJsonFgBase.java
@@ -7,6 +7,8 @@
  */
 package de.ii.ogcapi.features.jsonfg.domain;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.azahnen.dagger.annotations.AutoMultiBind;
 import com.google.common.collect.ImmutableSortedSet;
 import de.ii.ogcapi.features.core.domain.FeatureFormatExtension;
@@ -31,6 +33,7 @@ import de.ii.xtraplatform.store.domain.entities.ValidationResult;
 import de.ii.xtraplatform.store.domain.entities.ValidationResult.MODE;
 import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
+import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
@@ -176,7 +179,7 @@ public interface FeaturesFormatJsonFgBase extends FeatureFormatExtension {
                     JsonFgGeometryType.getGeometryDimension(
                         p.getGeometryType().orElse(SimpleFeatureGeometry.NONE),
                         p.getConstraints().map(SchemaConstraints::isComposite).orElse(false),
-                        p.getConstraints().map(SchemaConstraints::isComposite).orElse(false)))
+                        p.getConstraints().map(SchemaConstraints::isClosed).orElse(false)))
             .flatMap(Optional::stream)
             .distinct()
             .collect(Collectors.toUnmodifiableList());
@@ -184,5 +187,41 @@ public interface FeaturesFormatJsonFgBase extends FeatureFormatExtension {
       return Optional.empty();
     }
     return Optional.of(dimensions.get(0));
+  }
+
+  @Override
+  default boolean supportsHitsOnly() {
+    return true;
+  }
+
+  @Override
+  default Optional<Long> getNumberMatched(Object content) {
+    return getMetadata(content, "numberMatched");
+  }
+
+  @Override
+  default Optional<Long> getNumberReturned(Object content) {
+    return getMetadata(content, "numberReturned");
+  }
+
+  private Optional<Long> getMetadata(Object content, String key) {
+    if (content instanceof byte[]) {
+      JsonNode jsonNode;
+      ObjectMapper mapper = new ObjectMapper();
+      try {
+        jsonNode = mapper.readTree((byte[]) content);
+      } catch (IOException e) {
+        throw new IllegalStateException(
+            String.format("Could not parse GeoJSON object: %s", e.getMessage()), e);
+      }
+      if (jsonNode.isObject()) {
+        jsonNode = jsonNode.get(key);
+        if (jsonNode.isNumber()) {
+          return Optional.of(jsonNode.longValue());
+        }
+      }
+    }
+
+    return Optional.empty();
   }
 }

commit 2a1a70cdc596164a8c9b1333d0f30bd35accaedf

fix null pointer exception when extracting numberMatched
The exception was introduced in #901 when the request is for a single feature.

ogcapi-draft/ogcapi-features-gltf/src/main/java/de/ii/ogcapi/features/gltf/app/FeatureEncoderGltf.java
@@ -114,7 +114,7 @@ public class FeatureEncoderGltf extends FeatureObjectEncoder<PropertyGltf, Featu
   private List<Integer> featureNodes;
   private final long transformerStart = System.nanoTime();
   private long processingStart;
-  private long featuresFetched;
+  private OptionalLong featuresFetched;
   private OptionalLong featuresMatched;
   private int featureCount;
   private long featuresDuration;
@@ -140,9 +140,9 @@ public class FeatureEncoderGltf extends FeatureObjectEncoder<PropertyGltf, Featu
   public void onStart(ModifiableContext context) {
     this.processingStart = System.nanoTime();
     if (transformationContext.isFeatureCollection()) {
-      featuresFetched = context.metadata().getNumberReturned().orElseThrow();
+      featuresFetched = context.metadata().getNumberReturned();
       featuresMatched = context.metadata().getNumberMatched();
-      if (featuresFetched > WARNING_THRESHOLD_FEATURES_PER_FILE) {
+      if (featuresFetched.orElse(0L) > WARNING_THRESHOLD_FEATURES_PER_FILE) {
         LOGGER.warn("Fetching a large number of features for a tile: {}", featuresFetched);
       }
       if (LOGGER.isTraceEnabled()) {
@@ -155,6 +155,9 @@ public class FeatureEncoderGltf extends FeatureObjectEncoder<PropertyGltf, Featu
             .getNumberReturned()
             .ifPresent(num -> LOGGER.trace("numberReturned {}", num));
       }
+    } else {
+      featuresFetched = OptionalLong.empty();
+      featuresMatched = OptionalLong.empty();
     }
 
     initNewModel();
@@ -213,7 +216,7 @@ public class FeatureEncoderGltf extends FeatureObjectEncoder<PropertyGltf, Featu
       String text =
           String.format(
               "glTF features fetched: %d, returned: %d, total duration: %dms, processing: %dms, feature processing: %dms, average feature processing: %dms, writing: %dms.",
-              featuresFetched,
+              featuresFetched.orElse(1L),
               featureCount,
               transformerDuration,
               processingDuration,
@@ -318,8 +321,8 @@ public class FeatureEncoderGltf extends FeatureObjectEncoder<PropertyGltf, Featu
                 transformationContext
                     .getApiData()
                     .getMetadata()
-                    .flatMap(ApiMetadata::getAttribution))
-            .putExtras("numberReturned", featuresFetched);
+                    .flatMap(ApiMetadata::getAttribution));
+    featuresFetched.ifPresent(n -> metadataBuilder.putExtras("numberReturned", n));
     featuresMatched.ifPresent(n -> metadataBuilder.putExtras("numberMatched", n));
 
     builder = ImmutableGltfAsset.builder().asset(metadataBuilder.build());

ogcapi-draft/ogcapi-features-gltf/src/main/java/de/ii/ogcapi/features/gltf/app/FeaturesFormatGltfBinary.java
@@ -406,7 +406,7 @@ public class FeaturesFormatGltfBinary implements FeatureFormatExtension {
           }
         }
       } catch (IOException e) {
-        return Optional.empty();
+        // ignore
       }
     }
     return Optional.empty();

ogcapi-draft/ogcapi-features-jsonfg/src/main/java/de/ii/ogcapi/features/jsonfg/domain/FeaturesFormatJsonFgBase.java
@@ -191,15 +191,14 @@ public interface FeaturesFormatJsonFgBase extends FeatureFormatExtension {
       ObjectMapper mapper = new ObjectMapper();
       try {
         jsonNode = mapper.readTree((byte[]) content);
-      } catch (IOException e) {
-        throw new IllegalStateException(
-            String.format("Could not parse GeoJSON object: %s", e.getMessage()), e);
-      }
-      if (jsonNode.isObject()) {
-        jsonNode = jsonNode.get(key);
-        if (jsonNode.isNumber()) {
-          return Optional.of(jsonNode.longValue());
+        if (Objects.nonNull(jsonNode) && jsonNode.isObject()) {
+          jsonNode = jsonNode.get(key);
+          if (Objects.nonNull(jsonNode) && jsonNode.isNumber()) {
+            return Optional.of(jsonNode.longValue());
+          }
         }
+      } catch (IOException e) {
+        // ignore
       }
     }
 

ogcapi-stable/ogcapi-features-core/src/main/java/de/ii/ogcapi/features/core/app/FeaturesCoreQueriesHandlerImpl.java
@@ -371,9 +371,10 @@ public class FeaturesCoreQueriesHandlerImpl implements FeaturesCoreQueriesHandle
                     "%s.%s",
                     Objects.isNull(featureId) ? collectionId : featureId,
                     outputFormat.getMediaType().fileExtension())),
-            HeaderItems.of(
-                sendResponseAsStream ? Optional.empty() : outputFormat.getNumberMatched(bytes),
-                sendResponseAsStream ? Optional.empty() : outputFormat.getNumberReturned(bytes)))
+            Objects.isNull(featureId) && !sendResponseAsStream
+                ? HeaderItems.of(
+                    outputFormat.getNumberMatched(bytes), outputFormat.getNumberReturned(bytes))
+                : HeaderItems.of())
         .entity(Objects.nonNull(bytes) ? bytes : streamingOutput)
         .build();
   }

ogcapi-stable/ogcapi-features-geojson/src/main/java/de/ii/ogcapi/features/geojson/app/FeaturesFormatGeoJson.java
@@ -350,15 +350,14 @@ public class FeaturesFormatGeoJson
       ObjectMapper mapper = new ObjectMapper();
       try {
         jsonNode = mapper.readTree((byte[]) content);
-      } catch (IOException e) {
-        throw new IllegalStateException(
-            String.format("Could not parse GeoJSON object: %s", e.getMessage()), e);
-      }
-      if (jsonNode.isObject()) {
-        jsonNode = jsonNode.get(key);
-        if (jsonNode.isNumber()) {
-          return Optional.of(jsonNode.longValue());
+        if (Objects.nonNull(jsonNode) && jsonNode.isObject()) {
+          jsonNode = jsonNode.get(key);
+          if (Objects.nonNull(jsonNode) && jsonNode.isNumber()) {
+            return Optional.of(jsonNode.longValue());
+          }
         }
+      } catch (IOException e) {
+        // ignore
       }
     }
 

ogcapi-stable/ogcapi-foundation/src/main/java/de/ii/ogcapi/foundation/domain/HeaderItems.java
@@ -22,4 +22,8 @@ public interface HeaderItems {
         .numberReturned(numberReturned)
         .build();
   }
+
+  static HeaderItems of() {
+    return new ImmutableHeaderItems.Builder().build();
+  }
 }