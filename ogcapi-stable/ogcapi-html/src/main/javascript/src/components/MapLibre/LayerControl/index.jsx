054bda0fe278695ce7e32d86c10009c8a4629746
PZahnen <124141114+PZahnen@users.noreply.github.com>
2023-07-17 14:39:31 +0200

    collapsible sublayers

--- ogcapi-stable/ogcapi-html/src/main/javascript/src/components/MapLibre/LayerControl/index.jsx
+++ ogcapi-stable/ogcapi-html/src/main/javascript/src/components/MapLibre/LayerControl/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Button, ButtonGroup } from "reactstrap";
+import { Collapse } from "reactstrap";
 import { useMaplibreUIEffect } from "react-maplibre-ui";
 
 const LayerControl = ({ layerGroups }) => {
   const [selected, setSelected] = useState(Object.keys(layerGroups));
+  const [open, setOpen] = useState([]);
 
   const onSelect = (name) => {
     const index = selected.indexOf(name);
@@ -16,13 +17,22 @@ const LayerControl = ({ layerGroups }) => {
     setSelected([...selected]);
   };
 
+  const onOpen = (name) => {
+    const index = open.indexOf(name);
+    if (index < 0) {
+      open.push(name);
+    } else {
+      open.splice(index, 1);
+    }
+    setOpen([...open]);
+  };
+
   useMaplibreUIEffect(
     ({ map }) => {
       Object.keys(layerGroups).forEach((name) => {
         layerGroups[name].forEach((layerId) => {
           if (map.getLayer(layerId)) {
-            const visible =
-              map.getLayoutProperty(layerId, "visibility") !== "none";
+            const visible = map.getLayoutProperty(layerId, "visibility") !== "none";
 
             if (visible && !selected.includes(name)) {
               map.setLayoutProperty(layerId, "visibility", "none");
@@ -38,30 +48,52 @@ const LayerControl = ({ layerGroups }) => {
 
   return (
     <div
+      className="accordion"
       id="layer-control"
-      style={{ position: "absolute", zIndex: 1, top: "10px", right: "50px" }}
+      style={{ position: "absolute", zIndex: 1, top: "40px", left: "30px" }}
     >
-      <ButtonGroup
-        vertical
-        style={{
-          backgroundColor: "white",
-          borderRadius: "0.25rem",
-        }}
-      >
-        {Object.keys(layerGroups).map((name) => (
-          <Button
-            color="secondary"
-            outline
-            onClick={(e) => {
-              e.target.blur();
-              onSelect(name);
-            }}
-            active={selected.includes(name)}
-          >
-            {name}
-          </Button>
-        ))}
-      </ButtonGroup>
+      {Object.keys(layerGroups).map((name) => (
+        <div className="accordion-item" key={name}>
+          <h2 className="accordion-header" id={name}>
+            <button
+              style={{
+                backgroundColor: "white",
+                borderRadius: "0.25rem",
+              }}
+              color="secondary"
+              outline
+              onClick={(e) => {
+                e.target.blur();
+                onSelect(name);
+                onOpen(name);
+              }}
+              active={selected.includes(name)}
+              className="accordion-button"
+              type="button"
+              data-mdb-toggle="collapse"
+              data-mdb-target={`#collapse-${name}`}
+              aria-expanded={open.includes(name)}
+              aria-controls={`collapse-${name}`}
+            >
+              {name}
+            </button>
+          </h2>
+          {layerGroups[name].map((subLayer) => (
+            <Collapse
+              isOpen={open.includes(name)}
+              id={`collapse-${subLayer}`}
+              key={subLayer}
+              className="accordion-collapse"
+              aria-labelledby={`heading-${subLayer}`}
+              data-mdb-parent="#layer-control"
+            >
+              <div className="accordion-body" id={`${subLayer}`}>
+                {subLayer}
+              </div>
+            </Collapse>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
--- ogcapi-stable/ogcapi-html/src/main/javascript/src/components/MapLibre/stories.jsx
+++ ogcapi-stable/ogcapi-html/src/main/javascript/src/components/MapLibre/stories.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import MapLibre from '.';
+import MapLibre from ".";
 
 export default {
-    title: '@ogcapi/html/MapLibre',
-    component: MapLibre,
+  title: "@ogcapi/html/MapLibre",
+  component: MapLibre,
 };
 
 const Template = (args) => <MapLibre {...args} />;
@@ -12,40 +12,55 @@ const Template = (args) => <MapLibre {...args} />;
 export const Plain = Template.bind({});
 
 Plain.args = {
-    // data: 'http://localhost:7080/rest/services/feuerwehr/v1/collections/governmentalservice/items?f=json',
-    styleUrl:
-        'http://localhost:7080/rest/services/feuerwehr/v1/styles/default?f=mbs',
+  styleUrl: "https://demo.ldproxy.net/daraa/styles/topographic-with-basemap?f=mbs",
+  layerGroupControl: {
+    TransportationGroundCrv: [
+      "transportationgroundcrv.0a",
+      "transportationgroundcrv.0b",
+      "transportationgroundcrv.1",
+      "transportationgroundcrv.2",
+      "transportationgroundcrv.3",
+      "transportationgroundcrv.4",
+      "transportationgroundcrv.5",
+      "transportationgroundcrv.6",
+      "transportationgroundcrv.7",
+      "transportationgroundcrv.8",
+      "transportationgroundcrv.9",
+    ],
+  },
+  /*styleUrl:
+        'http://localhost:7080/rest/services/feuerwehr/v1/styles/default?f=mbs', */
 };
 
 export const Items = Template.bind({});
 
 Items.args = {
-    dataUrl:
-        'http://localhost:7080/rest/services/feuerwehr/v1/collections/governmentalservice/items?f=json',
+  dataUrl:
+    "http://localhost:7080/rest/services/feuerwehr/v1/collections/governmentalservice/items?f=json",
 };
 
 export const Extent = Template.bind({});
 
 Extent.args = {
-    bounds: [
-        [6.3, 49.0],
-        [8.4, 50.6],
-    ],
-    drawBounds: true,
-    interactive: false,
-    defaultStyle: {
-        color: 'red',
-    },
+  bounds: [
+    [6.3, 49.0],
+    [8.4, 50.6],
+  ],
+  drawBounds: true,
+  interactive: false,
+  defaultStyle: {
+    color: "red",
+  },
 };
 
 export const Tiles = Template.bind({});
 
 Tiles.args = {
-    dataUrl:
-        'http://localhost:7080/rest/services/feuerwehr/v1/tiles/WebMercatorQuad/{z}/{y}/{x}?f=mvt',
-    dataType: 'vector',
-    dataLayers: { governmentalservice: ['points'] },
-    // styleUrl:
-    //    'http://localhost:7080/rest/services/feuerwehr/v1/styles/default?f=mbs',
-    popup: 'CLICK_PROPERTIES',
+  dataUrl:
+    "http://localhost:7080/rest/services/feuerwehr/v1/tiles/WebMercatorQuad/{z}/{y}/{x}?f=mvt",
+  dataType: "vector",
+  dataLayers: { governmentalservice: ["points"] },
+  // styleUrl:
+  //    'http://localhost:7080/rest/services/feuerwehr/v1/styles/default?f=mbs',
+  popup: "CLICK_PROPERTIES",
 };
