interactive-instruments/ldproxy

commit 300ee7ef69e3c201d7cc63e10749c8651a27cf99

Transformation pipeline (#1091)
* upgrade xtraplatform

* improve rel profiles

* Schema: do not apply GeoJSON transformations

This was a leftover from the previous approach to schemas, where the schema was the GeoJSON schema. With the new OGC API Features Part 5 the GeoJSON transformations should not be taken into account when generating the schema resource.

* fix merge of branches

* fix npe

* fix tests (WIP)

there are still issues and the tests should be extended to cover the new cases

* fix feature refs as queryables

* fix npe

* fixes

* FEATURE_REF: adjust GML output

* fix feature refs as queryables

* revert first approach

* cleanup

* adjust to SchemaDeriver change

move title and description in withArrayWrapper to the array schema, if the property itself is an array

* improve queryables/sortables HTML view

show array properties in square brackets in the queryables/sortables HTML view

* adjust schema derivation

* default JSON Schema version: 2020-12

aligns with Part 5; also use "json" as a f value to align with queryables and sortables

* fix writing coalesce values in GeoJSON

* update tests (WIP)

* update unit tests

---------

Co-authored-by: Clemens Portele <portele@interactive-instruments.de>

ogcapi-stable/ogcapi-collections-queryables/src/main/java/de/ii/ogcapi/collections/queryables/app/QueryParameterTemplateQueryable.java
@@ -135,18 +135,16 @@ public abstract class QueryParameterTemplateQueryable extends ApiExtensionCache
       return null;
     }
 
-    switch (getType()) {
+    switch (getValueType().orElse(getType())) {
       case INTEGER:
       case FLOAT:
       case BOOLEAN:
         return Eq.of(getName(), getScalarLiteral(value, getType()));
-      case FEATURE_REF:
       case STRING:
         if (value.contains("*")) {
           return Like.of(getName(), ScalarLiteral.of(value.replaceAll("\\*", "%")));
         }
         return Eq.of(getName(), ScalarLiteral.of(value));
-      case FEATURE_REF_ARRAY:
       case VALUE_ARRAY:
         return AEquals.of(
             Property.of(getName()),

ogcapi-custom/ogcapi-features-geojson-ld/src/main/java/de/ii/ogcapi/features/geojson/ld/app/GeoJsonWriterJsonLd.java
@@ -120,7 +120,7 @@ public class GeoJsonWriterJsonLd implements GeoJsonWriter {
   public void onValue(
       EncodingAwareContextGeoJson context, Consumer<EncodingAwareContextGeoJson> next)
       throws IOException {
-    if (context.schema().isPresent() || Objects.nonNull(context.value())) {
+    if (context.schema().isPresent() && Objects.nonNull(context.value())) {
 
       final FeatureSchema currentSchema = context.schema().get();
       String currentValue = context.value();

ogcapi-draft/ogcapi-collections-schema/src/main/java/de/ii/ogcapi/collections/schema/app/SchemaCacheFeatures.java
@@ -11,13 +11,11 @@ import de.ii.ogcapi.features.core.domain.JsonSchemaCache;
 import de.ii.ogcapi.features.core.domain.JsonSchemaDocument;
 import de.ii.ogcapi.features.core.domain.JsonSchemaDocument.VERSION;
 import de.ii.ogcapi.features.core.domain.SchemaDeriverFeatures;
-import de.ii.ogcapi.features.geojson.domain.GeoJsonConfiguration;
 import de.ii.ogcapi.foundation.domain.FeatureTypeConfigurationOgcApi;
 import de.ii.ogcapi.foundation.domain.OgcApiDataV2;
 import de.ii.xtraplatform.codelists.domain.Codelist;
 import de.ii.xtraplatform.features.domain.FeatureSchema;
 import de.ii.xtraplatform.features.domain.SchemaBase;
-import de.ii.xtraplatform.features.domain.transform.PropertyTransformations;
 import de.ii.xtraplatform.features.domain.transform.WithScope;
 import de.ii.xtraplatform.features.domain.transform.WithTransformationsApplied;
 import java.util.EnumSet;
@@ -28,6 +26,8 @@ import java.util.function.Supplier;
 public class SchemaCacheFeatures extends JsonSchemaCache {
 
   private final Supplier<Map<String, Codelist>> codelistSupplier;
+  private static final WithTransformationsApplied WITH_TRANSFORMATIONS_APPLIED =
+      new WithTransformationsApplied();
   private static final WithScope WITH_SCOPE_SCHEMA =
       new WithScope(EnumSet.of(SchemaBase.Scope.RETURNABLE, SchemaBase.Scope.RECEIVABLE));
 
@@ -44,16 +44,6 @@ public class SchemaCacheFeatures extends JsonSchemaCache {
       Optional<String> schemaUri,
       VERSION version) {
 
-    Optional<PropertyTransformations> propertyTransformations =
-        collectionData
-            .getExtension(GeoJsonConfiguration.class)
-            .map(geoJsonConfiguration -> (PropertyTransformations) geoJsonConfiguration);
-
-    WithTransformationsApplied schemaTransformer =
-        propertyTransformations
-            .map(WithTransformationsApplied::new)
-            .orElse(new WithTransformationsApplied());
-
     SchemaDeriverFeatures schemaDeriverFeatures =
         new SchemaDeriverFeatures(
             version,
@@ -63,6 +53,9 @@ public class SchemaCacheFeatures extends JsonSchemaCache {
             codelistSupplier.get());
 
     return (JsonSchemaDocument)
-        schema.accept(schemaTransformer).accept(WITH_SCOPE_SCHEMA).accept(schemaDeriverFeatures);
+        schema
+            .accept(WITH_SCOPE_SCHEMA)
+            .accept(WITH_TRANSFORMATIONS_APPLIED)
+            .accept(schemaDeriverFeatures);
   }
 }

ogcapi-draft/ogcapi-collections-schema/src/main/java/de/ii/ogcapi/collections/schema/app/SchemaFormatJsonSchema202012.java
@@ -29,7 +29,7 @@ public class SchemaFormatJsonSchema202012 implements SchemaFormatExtension {
               new MediaType(
                   "application", "schema+json", ImmutableMap.of("schema", VERSION.V202012.url())))
           .label("JSON Schema (2020-12)")
-          .parameter("jsonschema")
+          .parameter("json")
           .fileExtension("schema.json")
           .build();
 

ogcapi-draft/ogcapi-features-gltf/src/main/java/de/ii/ogcapi/features/gltf/app/PropertyGltf.java
@@ -15,7 +15,6 @@ import de.ii.ogcapi.features.html.domain.Geometry;
 import de.ii.xtraplatform.features.domain.FeatureSchema;
 import de.ii.xtraplatform.features.domain.PropertyBase;
 import de.ii.xtraplatform.features.domain.SchemaBase;
-import de.ii.xtraplatform.features.domain.transform.FeaturePropertyTransformerFlatten;
 import de.ii.xtraplatform.geometries.domain.SimpleFeatureGeometry;
 import java.util.List;
 import java.util.Objects;
@@ -105,11 +104,6 @@ public interface PropertyGltf extends PropertyBase<PropertyGltf, FeatureSchema> 
                     .filter(Optional::isPresent)
                     .map(Optional::get)
                     .findFirst());
-  }
-
-  @Value.Lazy
-  default boolean isFlattened() {
-    return getTransformed().containsKey(FeaturePropertyTransformerFlatten.TYPE);
   }
 
   @Value.Lazy

ogcapi-draft/ogcapi-routes/src/main/java/de/ii/ogcapi/routes/app/encoder/PropertyRoutes.java
@@ -13,7 +13,6 @@ import de.ii.ogcapi.features.core.domain.Geometry;
 import de.ii.xtraplatform.features.domain.FeatureSchema;
 import de.ii.xtraplatform.features.domain.PropertyBase;
 import de.ii.xtraplatform.features.domain.SchemaBase;
-import de.ii.xtraplatform.features.domain.transform.FeaturePropertyTransformerFlatten;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -67,11 +66,6 @@ public interface PropertyRoutes extends PropertyBase<PropertyRoutes, FeatureSche
                     .filter(Optional::isPresent)
                     .map(Optional::get)
                     .findFirst());
-  }
-
-  @Value.Lazy
-  default boolean isFlattened() {
-    return getTransformed().containsKey(FeaturePropertyTransformerFlatten.TYPE);
   }
 
   Splitter PATH_SPLITTER = Splitter.on('.').omitEmptyStrings();

ogcapi-stable/ogcapi-features-core/src/main/java/de/ii/ogcapi/features/core/domain/FeatureFormatExtension.java
@@ -9,6 +9,7 @@ package de.ii.ogcapi.features.core.domain;
 
 import com.github.azahnen.dagger.annotations.AutoMultiBind;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import de.ii.ogcapi.foundation.domain.ApiMediaType;
 import de.ii.ogcapi.foundation.domain.ApiMediaTypeContent;
 import de.ii.ogcapi.foundation.domain.FeatureTypeConfigurationOgcApi;
@@ -18,13 +19,25 @@ import de.ii.xtraplatform.crs.domain.EpsgCrs;
 import de.ii.xtraplatform.features.domain.FeatureSchema;
 import de.ii.xtraplatform.features.domain.FeatureTokenEncoder;
 import de.ii.xtraplatform.features.domain.SchemaBase;
+import de.ii.xtraplatform.features.domain.transform.FeatureRefResolver;
 import de.ii.xtraplatform.features.domain.transform.ImmutablePropertyTransformation;
 import de.ii.xtraplatform.features.domain.transform.PropertyTransformations;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
 
 @AutoMultiBind
 public interface FeatureFormatExtension extends FormatExtension {
+
+  String URI_TEMPLATE =
+      String.format(
+          "{{%s | orElse:'{{apiUri}}/collections/%s/items/%s'}}",
+          FeatureRefResolver.URI_TEMPLATE, FeatureRefResolver.SUB_TYPE, FeatureRefResolver.SUB_ID);
+
+  String KEY_TEMPLATE =
+      String.format(
+          "{{%s | orElse:'%s::%s'}}",
+          FeatureRefResolver.KEY_TEMPLATE, FeatureRefResolver.SUB_TYPE, FeatureRefResolver.SUB_ID);
 
   ApiMediaType getCollectionMediaType();
 
@@ -106,34 +119,29 @@ public interface FeatureFormatExtension extends FormatExtension {
     }
 
     ImmutableProfileTransformations.Builder builder = new ImmutableProfileTransformations.Builder();
-    switch (profile.get()) {
-      default:
-      case AS_KEY:
-        // nothing to transform
-        return getPropertyTransformations(collectionData);
-      case AS_URI:
-      case AS_LINK:
-        // TODO: Currently there is no PropertyTransformation to convert a FEATURE_REF to a Link
-        //       object, so we just map it to the URI for now. For now, the Link object must be
-        //       handled in the feature encoders of the formats that support 'rel-as-link'.
-        schema
-            .map(SchemaBase::getAllNestedProperties)
-            .ifPresent(
-                properties ->
-                    properties.stream()
-                        .filter(SchemaBase::isFeatureRef)
-                        .forEach(
-                            property ->
-                                getTemplate(property)
-                                    .ifPresent(
-                                        template ->
-                                            builder.putTransformations(
-                                                property.getFullPathAsString(),
-                                                ImmutableList.of(
-                                                    new ImmutablePropertyTransformation.Builder()
-                                                        .stringFormat(template)
-                                                        .build())))));
-    }
+
+    schema
+        .map(SchemaBase::getAllNestedProperties)
+        .ifPresent(
+            properties ->
+                properties.stream()
+                    .filter(SchemaBase::isFeatureRef)
+                    .forEach(
+                        property -> {
+                          switch (profile.get()) {
+                            default:
+                            case AS_KEY:
+                              reduceToKey(property, builder);
+                              break;
+                            case AS_URI:
+                              reduceToUri(property, builder);
+                              break;
+                            case AS_LINK:
+                              mapToLink(property, builder);
+                              break;
+                          }
+                        }));
+
     ProfileTransformations profileTransformations = builder.build();
     return Optional.of(
         getPropertyTransformations(collectionData)
@@ -141,22 +149,42 @@ public interface FeatureFormatExtension extends FormatExtension {
             .orElse(profileTransformations));
   }
 
-  static Optional<String> getTemplate(FeatureSchema property) {
-    return Optional.ofNullable(
-        property
-            .getRefUriTemplate()
-            .orElse(
-                property
-                    .getRefType()
-                    .map(
-                        refType ->
-                            String.format("{{apiUri}}/collections/%s/items/{{value}}", refType))
-                    .or(
-                        () ->
-                            property.getConcat().isEmpty() && property.getCoalesce().isEmpty()
-                                ? Optional.empty()
-                                : Optional.of("{{apiUri}}/collections/{{type}}/items/{{value}}"))
-                    .orElse(null)));
+  static void reduceToKey(FeatureSchema schema, ImmutableProfileTransformations.Builder builder) {
+    builder.putTransformations(
+        schema.getFullPathAsString(),
+        ImmutableList.of(
+            schema
+                        .getRefType()
+                        .filter(
+                            refType ->
+                                !Objects.equals(refType, FeatureRefResolver.REF_TYPE_DYNAMIC))
+                        .isPresent()
+                    && schema.getRefKeyTemplate().isEmpty()
+                ? new ImmutablePropertyTransformation.Builder()
+                    .objectReduceSelect(FeatureRefResolver.ID)
+                    .build()
+                : new ImmutablePropertyTransformation.Builder()
+                    .objectReduceFormat(KEY_TEMPLATE)
+                    .build()));
+  }
+
+  static void reduceToUri(FeatureSchema schema, ImmutableProfileTransformations.Builder builder) {
+    builder.putTransformations(
+        schema.getFullPathAsString(),
+        ImmutableList.of(
+            new ImmutablePropertyTransformation.Builder()
+                .objectReduceFormat(URI_TEMPLATE)
+                .build()));
+  }
+
+  static void mapToLink(FeatureSchema schema, ImmutableProfileTransformations.Builder builder) {
+    builder.putTransformations(
+        schema.getFullPathAsString(),
+        ImmutableList.of(
+            new ImmutablePropertyTransformation.Builder()
+                .objectMapFormat(
+                    ImmutableMap.of("title", FeatureRefResolver.SUB_TITLE, "href", URI_TEMPLATE))
+                .build()));
   }
 
   default boolean supportsHitsOnly() {

ogcapi-stable/ogcapi-features-core/src/main/java/de/ii/ogcapi/features/core/domain/JsonSchemaDocument.java
@@ -35,7 +35,7 @@ public abstract class JsonSchemaDocument extends JsonSchemaObject {
     V7("http://json-schema.org/draft-07/schema#", "definitions");
 
     public static VERSION current() {
-      return V201909;
+      return V202012;
     }
 
     private final String url;

commit 958a2ea85630b7c96f629e7be81438e7ea4da6d2

fix array queryables
this fixes an issue introduced in #1091

ogcapi-stable/ogcapi-collections-queryables/src/main/java/de/ii/ogcapi/collections/queryables/app/QueryParameterTemplateQueryable.java
@@ -135,23 +135,27 @@ public abstract class QueryParameterTemplateQueryable extends ApiExtensionCache
       return null;
     }
 
-    switch (getValueType().orElse(getType())) {
+    // handle array case
+    if (getType() == Type.VALUE_ARRAY) {
+      return AEquals.of(
+          Property.of(getName()),
+          ArrayLiteral.of(
+              ARRAY_SPLITTER.splitToList(value).stream()
+                  .map(s -> getScalarLiteral(s, getValueType().orElse(Type.STRING)))
+                  .collect(Collectors.toUnmodifiableList())));
+    }
+
+    Type t = getValueType().orElse(getType());
+    switch (t) {
       case INTEGER:
       case FLOAT:
       case BOOLEAN:
-        return Eq.of(getName(), getScalarLiteral(value, getType()));
+        return Eq.of(getName(), getScalarLiteral(value, t));
       case STRING:
         if (value.contains("*")) {
           return Like.of(getName(), ScalarLiteral.of(value.replaceAll("\\*", "%")));
         }
         return Eq.of(getName(), ScalarLiteral.of(value));
-      case VALUE_ARRAY:
-        return AEquals.of(
-            Property.of(getName()),
-            ArrayLiteral.of(
-                ARRAY_SPLITTER.splitToList(value).stream()
-                    .map(s -> getScalarLiteral(s, getValueType().orElse(Type.STRING)))
-                    .collect(Collectors.toUnmodifiableList())));
       default:
         return BooleanValue2.of(false);
     }