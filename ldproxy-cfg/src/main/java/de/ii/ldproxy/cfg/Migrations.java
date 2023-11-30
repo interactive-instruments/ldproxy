a6fdb195137c41fa0d9ec13edd0cbf7310132dd0
Clemens Portele <portele@interactive-instruments.de>
2023-11-30 14:31:32 +0100

    add StylesMigrationV4

--- ldproxy-cfg/src/main/java/de/ii/ldproxy/cfg/Migrations.java
+++ ldproxy-cfg/src/main/java/de/ii/ldproxy/cfg/Migrations.java
@@ -7,6 +7,7 @@
  */
 package de.ii.ldproxy.cfg;
 
+import de.ii.ogcapi.resources.app.StylesMigrationV4;
 import de.ii.ogcapi.text.search.app.QueryablesMigrationV4;
 import de.ii.ogcapi.tiles.domain.TilesMigrationV4;
 import de.ii.xtraplatform.entities.domain.EntityDataStore;
@@ -19,7 +20,11 @@ public interface Migrations {
   static Migrations create(EntityDataStore<?> entityDataStore) {
     EntityMigrationContext context = entityDataStore::has;
 
-    return () -> List.of(new TilesMigrationV4(context), new QueryablesMigrationV4(context));
+    return () ->
+        List.of(
+            new TilesMigrationV4(context),
+            new QueryablesMigrationV4(context),
+            new StylesMigrationV4(context));
   }
 
   List<EntityMigration<?, ?>> entity();
