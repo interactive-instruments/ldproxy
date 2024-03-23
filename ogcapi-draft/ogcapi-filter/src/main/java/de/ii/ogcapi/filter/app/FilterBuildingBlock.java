96b4f8f52a8c08a0c617085a1afc50826e5c05d9
Clemens Portele <portele@interactive-instruments.de>
2024-03-23 08:19:08 +0100

    update documentation

--- ogcapi-draft/ogcapi-filter/src/main/java/de/ii/ogcapi/filter/app/FilterBuildingBlock.java
+++ ogcapi-draft/ogcapi-filter/src/main/java/de/ii/ogcapi/filter/app/FilterBuildingBlock.java
@@ -22,39 +22,67 @@ import javax.inject.Singleton;
  * @langEn Filter features with CQL2 expressions.
  * @langDe Features mit CQL2-Ausdrücken filtern.
  * @scopeEn This module provides query parameters to filter features using CQL2 (in Text or JSON
- *     encoding). Depending on the feature provider, some capabilities of CQL2 may not be supported.
- *     Specifically, in GeoPackage feature providers, queryables in a JSON column that are arrays
- *     are not supported. In PostgreSQL/PostGIS feature providers, the `A_OVERLAPS` operator is not
- *     supported for queryables in a JSON column.
+ *     encoding).
+ *     <p>In addition to the standard functions specified in CQL2, the following custom functions
+ *     are supported, too: <code>
+ * - `UPPER(String): String` returns the string in upper case.
+ * - `LOWER(String): String` returns the string in lower case.
+ * - `NOW(): Timestamp` returns the current time.
+ * - `DIAMETER2D(Geometry): Double` returns the diameter of a geometry with 2D coordinates.
+ * - `DIAMETER3D(Geometry): Double` returns the diameter of a geometry with 3D coordinates.
+ *     </code>
  * @scopeDe Dieses Modul bietet Abfrageparameter zum Filtern von Features mit CQL2 (in Text- oder
- *     JSON-Kodierung). Je nach Feature-Provider werden einige Funktionen von CQL2 möglicherweise
- *     nicht unterstützt. Insbesondere werden in GeoPackage Feature-Providern Queryables in einer
- *     JSON-Spalte, die Arrays sind, nicht unterstützt. In PostgreSQL/PostGIS-Feature-Anbietern wird
- *     der Operator `A_OVERLAPS` für Queryables in einer JSON-Spalte nicht unterstützt.
+ *     JSON-Kodierung).
+ *     <p>Zusätzlich zu den in CQL2 spezifizierten Standardfunktionen werden auch die folgenden
+ *     benutzerdefinierten Funktionen unterstützt: <code>
+ * - `UPPER(String): String` gibt die Zeichenkette in Großbuchstaben zurück.
+ * - `LOWER(String): String` gibt die Zeichenkette in Kleinbuchstaben zurück.
+ * - `NOW(): Timestamp` gibt die aktuelle Zeit zurück.
+ * - `DIAMETER2D(Geometry): Double` gibt den Durchmesser einer Geometrie mit 2D-Koordinaten zurück.
+ * - `DIAMETER3D(Geometry): Double` gibt den Durchmesser einer Geometrie mit 3D-Koordinaten zurück.
+ *     </code>
  * @conformanceEn This module implements requirements of the conformance classes *Filter* and
  *     *Features Filter* from the draft specification [OGC API - Features - Part 3: Common Query
- *     Language](https://docs.ogc.org/DRAFTS/19-079r1.html) as well as the conformance classes
+ *     Language](https://docs.ogc.org/DRAFTS/19-079r2.html) as well as the conformance classes
  *     *Basic CQL2*, *Advanced Comparison Operators*, *Case-insensitive Comparisons*,
- *     *Accent-insensitive Comparisons*, *Basic Spatial Operators*, *Spatial Operators*, *Temporal
- *     Operators*, *Array Operators*, *Property-Property Comparisons*, *CQL2 Text encoding*, and
- *     *CQL2 JSON encoding* from the draft specification [Common Query Language
- *     (CQL2](https://docs.ogc.org/DRAFTS/21-065.html). The implementation is subject to change in
- *     the course of the development and approval process of the draft.
+ *     *Accent-insensitive Comparisons*, *Basic Spatial Functions*, *Basic Spatial Functions with
+ *     additional Spatial Data Types*, *Spatial Functions*, *Temporal Functions*, *Array Functions*,
+ *     *Property-Property Comparisons*, *CQL2 Text encoding*, and *CQL2 JSON encoding* from the
+ *     draft specification [Common Query Language (CQL2](https://docs.ogc.org/DRAFTS/21-065r1.html).
+ *     The implementation is subject to change in the course of the development and approval process
+ *     of the draft.
  *     <p>The publication of queryables is controlled via [Feature Collections -
  *     Queryables](feature_collections_-_queryables.md) and is a prerequisite for clients to be able
  *     to determine the queryable feature properties.
  * @conformanceDe Dieses Modul implementiert die Anforderungen der Konformitätsklassen *Filter* und
  *     *Features Filter* aus dem Entwurf der Spezifikation [OGC API - Features - Part 3: Common
- *     Query Language](https://docs.ogc.org/DRAFTS/19-079r1.html) sowie die Konformitätsklassen
+ *     Query Language](https://docs.ogc.org/DRAFTS/19-079r2.html) sowie die Konformitätsklassen
  *     *Basic CQL2*, *Advanced Comparison Operators*, *Case-insensitive Comparisons*,
- *     *Accent-insensitive Comparisons*, *Basic Spatial Operators*, *Spatial Operators*, *Temporal
- *     Operators*, *Array Operators*, *Property-Property Comparisons*, *CQL2 Text encoding*, und
- *     *CQL2 JSON encoding* aus dem Entwurf der Spezifikation [Common Query Language
- *     (CQL2](https://docs.ogc.org/DRAFTS/21-065.html). Die Implementierung wird sich im Zuge der
+ *     *Accent-insensitive Comparisons*, *Basic Spatial Functions*, *Basic Spatial Functions with
+ *     additional Spatial Data Types*, *Spatial Functions*, *Temporal Functions*, *Array Functions*,
+ *     *Property-Property Comparisons*, *CQL2 Text encoding*, und *CQL2 JSON encoding* aus dem
+ *     Entwurf der Spezifikation [Common Query Language
+ *     (CQL2](https://docs.ogc.org/DRAFTS/21-065r1.html). Die Implementierung wird sich im Zuge der
  *     weiteren Standardisierung der Spezifikation noch ändern.
  *     <p>Die Veröffentlichung der Queryables wird über [Feature Collections -
  *     Queryables](feature_collections_-_queryables.md) gesteuert und ist Voraussetzung, damit
  *     Clients die abfragbaren Objekteigenschaften bestimmen können.
+ * @limitationsEn Depending on the feature provider, some capabilities of CQL2 may not be supported.
+ *     Specifically, in GeoPackage feature providers, queryables in a JSON column that are arrays
+ *     are not supported. In PostgreSQL/PostGIS feature providers, the `A_OVERLAPS` operator is not
+ *     supported for queryables in a JSON column.
+ *     <p>The conformance classes *Functions* and *Arithmetic Expressions* are not supported.
+ *     <p>The operator `IN` requires a property on the left side and literals on the right side.
+ *     <p>Only `booleanLiteral` may be used from `booleanExpression` in a `scalarExpression`.
+ * @limitationsDe Je nach Feature-Provider werden einige Funktionen von CQL2 möglicherweise nicht
+ *     unterstützt. Insbesondere werden in GeoPackage Feature-Providern Queryables in einer
+ *     JSON-Spalte, die Arrays sind, nicht unterstützt. In PostgreSQL/PostGIS-Feature-Anbietern wird
+ *     der Operator `A_OVERLAPS` für Queryables in einer JSON-Spalte nicht unterstützt.
+ *     <p>Die Konformitätsklassen *Functions* und *Arithmetic Expressions* werden nicht unterstützt.
+ *     <p>In einer `scalarExpression` darf aus `booleanExpression` nur `booleanLiteral` verwendet
+ *     werden.
+ *     <p>Der Operator `IN` erfordert eine Eigenschaft auf der linken Seite und Literale auf der
+ *     rechten Seite.
  * @ref:cfg {@link de.ii.ogcapi.filter.domain.FilterConfiguration}
  * @ref:cfgProperties {@link de.ii.ogcapi.filter.domain.ImmutableFilterConfiguration}
  * @ref:queryParameters {@link de.ii.ogcapi.filter.api.QueryParameterFilter}, {@link
@@ -70,7 +98,7 @@ public class FilterBuildingBlock implements ApiBuildingBlock {
   public static final Optional<ExternalDocumentation> SPEC =
       Optional.of(
           ExternalDocumentation.of(
-              "https://docs.ogc.org/DRAFTS/19-079r1.html",
+              "https://docs.ogc.org/DRAFTS/19-079r2.html",
               "OGC API - Features - Part 3: Filtering (DRAFT)"));
 
   @Inject
