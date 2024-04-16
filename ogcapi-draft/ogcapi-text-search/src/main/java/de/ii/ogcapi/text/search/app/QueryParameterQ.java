19c0b3ba1a07193c37ff6e51a9fcd8104a53524a
Clemens Portele <portele@interactive-instruments.de>
2024-04-16 20:15:04 +0200

    fix documentation (#1204)
    The reference to the case limitation was only removed in the API definition, not in the ldproxy documentation.

--- ogcapi-draft/ogcapi-text-search/src/main/java/de/ii/ogcapi/text/search/app/QueryParameterQ.java
+++ ogcapi-draft/ogcapi-text-search/src/main/java/de/ii/ogcapi/text/search/app/QueryParameterQ.java
@@ -50,13 +50,10 @@ import javax.inject.Singleton;
  * @endpoints Features
  * @langEn General text search in multiple text properties of the data. Search terms are separated
  *     by comma. If at least one of the search terms is included in an item, it is included in the
- *     result set. Known limitation: The search should be case-insensitive, but currently is
- *     case-sensitive.
+ *     result set.
  * @langDe Allgemeine Textsuche in mehreren Texteigenschaften der Daten. Die Suchbegriffe werden
  *     durch ein Komma getrennt. Wenn mindestens einer der Suchbegriffe in einem Objekt enthalten
- *     ist, wird er in die Ergebnismenge aufgenommen. Bekannte Einschränkung: Die Suche sollte Groß-
- *     und Kleinschreibung nicht berücksichtigen, derzeit wird jedoch zwischen Groß- und
- *     Kleinschreibung unterschieden.
+ *     ist, wird er in die Ergebnismenge aufgenommen.
  */
 @Singleton
 @AutoBind
