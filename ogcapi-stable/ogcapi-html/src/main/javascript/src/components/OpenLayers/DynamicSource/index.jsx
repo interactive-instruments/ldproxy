3dd0521f95fee383e06551380bdf9a1820a0feaf
Andreas Zahnen <zahnen@interactive-instruments.de>
2024-05-08 08:50:15 +0200

    support raster tiles in openlayers

--- ogcapi-stable/ogcapi-html/src/main/javascript/src/components/OpenLayers/DynamicSource/index.jsx
+++ ogcapi-stable/ogcapi-html/src/main/javascript/src/components/OpenLayers/DynamicSource/index.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { RContext } from "rlayers";
-import { VectorTile as VectorTileSource } from "ol/source";
+import { VectorTile as VectorTileSource, XYZ as XYZSource } from "ol/source";
 import TileGrid from "ol/tilegrid/TileGrid";
 import { MVT } from "ol/format";
 
-const DynamicView = ({ tileMatrixSet, dataUrl, update }) => (
+const DynamicView = ({ tileMatrixSet, dataUrl, dataType, update }) => (
   <RContext.Consumer>
     {({ layer }) => {
       if (update && tileMatrixSet) {
         layer.set(
           "source",
-          new VectorTileSource({
+          dataType === "raster"
+          ? new XYZSource({
+              url: dataUrl.replace(
+                "WebMercatorQuad",
+                tileMatrixSet.tileMatrixSet
+              ),
+              maxZoom: tileMatrixSet.maxLevel,
+              projection: tileMatrixSet.projection,
+              tileGrid: new TileGrid({
+                extent: JSON.parse(tileMatrixSet.extent),
+                resolutions: JSON.parse(tileMatrixSet.resolutions),
+                sizes: JSON.parse(tileMatrixSet.sizes),
+              }),
+            })
+          : new VectorTileSource({
             url: dataUrl.replace(
               "WebMercatorQuad",
               tileMatrixSet.tileMatrixSet
@@ -35,12 +49,14 @@ DynamicView.displayName = "DynamicView";
 DynamicView.propTypes = {
   tileMatrixSet: PropTypes.objectOf(PropTypes.string),
   dataUrl: PropTypes.string,
+  dataType: PropTypes.string,
   update: PropTypes.bool,
 };
 
 DynamicView.defaultProps = {
   tileMatrixSet: null,
   dataUrl: "",
+  dataType: null,
   update: false,
 };
 
